d991d0564a875c3432da17f19c7cf857ccc2d173
Anton Stekanov <anton0xf@yandex-team.ru>
2020-04-26 18:18:00 +0500

    gigasecond: done

--- racket/gigasecond/gigasecond.rkt
+++ racket/gigasecond/gigasecond.rkt
@@ -1,6 +1,11 @@
 #lang racket
-
 (provide add-gigasecond)
+(require racket/date
+         threading)
 
-(require racket/date)
+(define gigasecond (expt 10 9))
 
+(define (add-gigasecond dt)
+  (~> (date->seconds dt)
+      (+ gigasecond)
+      (seconds->date)))
