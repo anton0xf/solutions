6fec4432e777f7cbc98bf4a9d0821f1bc86b603c
Anton Stekanov <anton0xf@yandex-team.ru>
2020-05-02 11:38:36 +0500

    01_map: recommended

--- 01_map.py
+++ 01_map.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+import string
+
+# rules = {'k': 'm', 'o': 'q', 'e': 'g'}
+# [ord(rules[c]) - ord(c) for c in rules] == [2, 2, 2]
+
 s = '''g fmnc wms bgblr rpylqjyrc gr zw fylb.
 rfyrq ufyr amknsrcpq ypc dmp.
 bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle.
 sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
-
-# rules = {'k': 'm', 'o': 'q', 'e': 'g'}
-# [ord(rules[c]) - ord(c) for c in rules] == [2, 2, 2]
 
 
 def map_char(c):
@@ -20,6 +22,23 @@ def map_char(c):
 def map_str(s):
     return ''.join(map_char(c) for c in s)
 
+# print(map_str(s))
+
+
+# recommended aproach
+abc = string.ascii_lowercase
+
+
+def shift(n, xs):
+    return xs[n:] + xs[:n]
+
+
+def trans(n, s):
+    cab = shift(n, abc)
+    tr = str.maketrans(abc, cab)
+    return s.translate(tr)
+
 
 if __name__ == '__main__':
-    print(map_str(s))
+    print(trans(2, s))
+    print(trans(2, 'map'))
