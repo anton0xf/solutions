bf54848e74816e4991f187bbbb10481b4b596f52
Anton Stekanov <anton0xf@yandex-team.ru>
2020-05-08 16:22:34 +0500

    pythons/hamming: done

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,4 @@
-def distance(strand_a, strand_b):
-    pass
+def distance(str_a, str_b):
+    if len(str_a) != len(str_b):
+        raise ValueError('String lengths differ')
+    return sum(1 for a, b in zip(str_a, str_b) if a != b)
