834383fc0e60784829e858a61cff814acc1e7a46
Anton Stekanov <anton0xf@yandex-team.ru>
2020-05-09 16:51:55 +0500

    learn/arrays/ex03_squares_of_a_sorted_array: two pointers approach

--- learn/arrays/ex03_squares_of_a_sorted_array_test.py
+++ learn/arrays/ex03_squares_of_a_sorted_array_test.py
@@ -9,3 +9,34 @@ def test_example1():
 
 def test_example2():
     assert [4, 9, 9, 49, 121] == s.sortedSquares([-7, -3, 2, 3, 11])
+
+
+def test_non_negative():
+    assert [0, 9, 100] == s.sortedSquares([0, 3, 10])
+
+
+def test_positive():
+    assert [9, 100] == s.sortedSquares([3, 10])
+
+
+def test_non_positive():
+    assert [0, 1, 16] == s.sortedSquares([-4, -1, 0])
+
+
+def test_negative():
+    assert [1, 16] == s.sortedSquares([-4, -1])
+
+
+def test_empty():
+    assert [] == s.sortedSquares([])
+
+
+def test_first_non_negative_index():
+    f = sol.first_non_negative_index
+    assert f([-1, 0, 1]) == 1
+    assert f([0, 1]) == 0
+    assert f([1]) == 0
+    assert f([-1]) == 1
+    assert f([-2, -1]) == 2
+    assert f([-2, -1, 0]) == 2
+    assert f([-2, -1, 0, 1]) == 2
