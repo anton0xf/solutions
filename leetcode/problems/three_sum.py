a85631fe7d984cb9a07ec37adf021eb0c0de6334
anton0xf <anton0xf@gmail.com>
2023-08-13 22:57:12 +0300

    lc: three sum: use dict

--- leetcode/problems/three_sum.py
+++ leetcode/problems/three_sum.py
@@ -7,36 +7,22 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         nums.sort()
-        res = set()
+        idxs = {n: i for i, n in enumerate(nums)}
+        res = []
         for i in range(n - 2):
             ni = nums[i]
             if ni > 0:
                 break
             if i > 0 and ni == nums[i-1]:
                 continue
-            j = i + 1
-            k = n - 1
-            while j < k:
+            for j in range(i+1, n-1):
                 nj = nums[j]
-                nk = nums[k]
-                s1 = ni + nj
-                if s1 > 0 or nk < 0:
+                if j > i+1 and nj == nums[j-1]:
+                    continue
+                s = ni + nj
+                if s > 0:
                     break
-                s = s1 + nk
-                if s == 0:
-                    res.add((ni, nj, nk))
-                    while j < k-1 and nj == nums[j+1]:
-                        j += 1
-                    j += 1
-                    while j < k-1 and nk == nums[k-1]:
-                        k -= 1
-                    k -= 1
-                elif s < 0:
-                    while j < k-1 and nj == nums[j+1]:
-                        j += 1
-                    j += 1
-                else:
-                    while j < k-1 and nk == nums[k-1]:
-                        k -= 1
-                    k -= 1
+                t = -s
+                if t in idxs and idxs[t] > j:
+                    res.append((ni, nj, t))
         return res
