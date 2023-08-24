ec9c9cd7309759d38d97c23dd5431ea10f901b5a
anton0xf <anton0xf@gmail.com>
2023-08-24 14:42:10 +0300

    lc: valid palindrome: even better memory but worser time

--- leetcode/problems/valid_palindrome.py
+++ leetcode/problems/valid_palindrome.py
@@ -8,3 +8,19 @@ class Solution:
             if chs[i] != chs[-i-1]:
                 return False
         return True
+
+    # better memory but worse time
+    def isPalindrome2(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
