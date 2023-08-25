dcb54c1c13e44468b74a30c73931262162f7ae9d
anton0xf <anton0xf@gmail.com>
2023-08-25 16:38:20 +0300

    lc: valid anagram: Counter

--- leetcode/problems/valid_anagram.py
+++ leetcode/problems/valid_anagram.py
@@ -1,15 +1,7 @@
 # https://leetcode.com/problems/valid-anagram/
-from collections import defaultdict
-from typing import Dict
-
-
-def count_chars(s: str) -> Dict[str, int]:
-    res = defaultdict(int)
-    for ch in s:
-        res[ch] += 1
-    return res
+from collections import Counter
 
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return count_chars(s) == count_chars(t)
+        return Counter(s) == Counter(t)
