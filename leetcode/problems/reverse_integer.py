1d6e7de49f76821dffa11d4f774048686fc0ec04
anton0xf <anton0xf@gmail.com>
2023-08-26 11:46:48 +0300

    lc: reverse_integer: bit_lenght()

--- leetcode/problems/reverse_integer.py
+++ leetcode/problems/reverse_integer.py
@@ -1,16 +1,9 @@
 # https://leetcode.com/problems/reverse-integer/
 
-MIN, MAX = -2**31, 2**31 - 1
-
-
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < 0:
-            res = -self._reverse(-x)
-            return res if res >= MIN else 0
-        else:
-            res = self._reverse(x)
-            return res if res <= MAX else 0
+        res = self._reverse(x) if x >= 0 else -self._reverse(-x)
+        return res if res.bit_length() <= 31 else 0
 
     def _reverse(self, x: int) -> int:
         res = 0
