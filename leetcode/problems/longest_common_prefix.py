0e4d41a0ea4b1a066d8345f4c5679c0422a964cb
anton0xf <anton0xf@gmail.com>
2023-08-26 11:11:30 +0300

    lc: longest common prefix: sort strs

--- leetcode/problems/longest_common_prefix.py
+++ leetcode/problems/longest_common_prefix.py
@@ -4,13 +4,14 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+        strs.sort()
+        s1, s2 = strs[0], strs[-1]
         res = ""
-        for n in range(200):
-            if n >= len(strs[0]):
-                break
-            ch = strs[0][n]
-            if all((n < len(s) and ch == s[n] for s in strs)):
-                res += ch
+        for i in range(min(len(s1), len(s2))):
+            if s1[i] == s2[i]:
+                res += s1[i]
             else:
                 break
         return res
