e08f3a34dfbd25bf7ad5d74e9d8055cd83ac7ad2
anton0xf <anton0xf@gmail.com>
2023-08-29 16:26:14 +0300

    lc: Minimum Penalty for a Shop: optimize

--- leetcode/problems/minimum_penalty_for_a_shop.py
+++ leetcode/problems/minimum_penalty_for_a_shop.py
@@ -1,30 +1,15 @@
 # https://leetcode.com/problems/minimum-penalty-for-a-shop/
-from operator import add
-
 
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         n = len(customers)
-
-        v = [int(ch == 'Y') for ch in customers]
-        v.append(0)
-
-        o = [0] * (n+1)
-        c = [0] * (n+1)
-        acc_o = acc_c = 0
-        for i in range(n):
-            acc_o += 1 - v[i]
-            o[i+1] = acc_o
-
-        for j in range(n, -1, -1):
-            acc_c += v[j]
-            c[j] += acc_c
-
-        res_v = n
-        res_i = 0
-        for i, s in enumerate(map(add, o, c)):
-            if s < res_v:
-                res_v = s
-                res_i = i
-
-        return res_i
+        vs = [int(ch == 'Y') for ch in customers]
+        p = sum(vs)
+        min_p = p
+        min_i = 0
+        for i in range(1, n+1):
+            p += 1 - 2*vs[i-1]
+            if p < min_p:
+                min_p = p
+                min_i = i
+        return min_i
