27ad8abf3afbd8f2f7bd4b8be3ae7fdeb459e46c
anton0xf <anton0xf@gmail.com>
2023-08-29 16:27:53 +0300

    lc: Minimum Penalty for a Shop: don't calculate right base penalty

--- leetcode/problems/minimum_penalty_for_a_shop.py
+++ leetcode/problems/minimum_penalty_for_a_shop.py
@@ -3,12 +3,10 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         n = len(customers)
-        vs = [int(ch == 'Y') for ch in customers]
-        p = sum(vs)
-        min_p = p
+        min_p = p = n
         min_i = 0
         for i in range(1, n+1):
-            p += 1 - 2*vs[i-1]
+            p += 1 - 2 * (customers[i-1] == 'Y')
             if p < min_p:
                 min_p = p
                 min_i = i
