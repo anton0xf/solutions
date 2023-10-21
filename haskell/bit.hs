5eb6d6de1842d02824271377334fef17aa65aac4
anton0xf <anton0xf@gmail.com>
2023-10-21 19:31:42 +0300

    haskell/stepik: 4.2.9 bitSum: direct solution, bitSum3 with test

--- haskell/bit.hs
+++ haskell/bit.hs
@@ -8,7 +8,10 @@
 Можно считать, что на вход не будут подаваться числа с ведущими нулями. 
 -}
 
-data Bit = Zero | One
+uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f (a, b, c) = f a b c
+
+data Bit = Zero | One deriving Eq
 instance Show Bit where
   show Zero = "0"
   show One = "1"
@@ -50,11 +53,70 @@ itoz :: Integer -> Z
 itoz n | n >= 0 = Z Plus (iton n)
        | otherwise = Z Minus (iton (-n))
 
-add :: Z -> Z -> Z
-add x y = itoz $ ztoi x + ztoi y
+add' :: Z -> Z -> Z
+add' x y = itoz $ ztoi x + ztoi y
 
--- ztoi $ itoz (-10) `add` itoz 9
+-- ztoi $ itoz (-10) `add'` itoz 9
 -- => -1
 
-mul :: Z -> Z -> Z
-mul x y = itoz $ ztoi x * ztoi y
+mul' :: Z -> Z -> Z
+mul' x y = itoz $ ztoi x * ztoi y
+
+-- direct approach
+
+bitSum :: Bit -> Bit -> (Bit, Bit)
+bitSum Zero Zero = (Zero, Zero)
+bitSum One One = (One, One)
+bitSum _ _ = (One, Zero)
+
+{- mapM_ (\x -> print (x, (uncurry bitSum) x)) [(a, b) | a <- bits, b <- bits]
+=>
+((0,0),(0,0))
+((0,1),(1,0))
+((1,0),(1,0))
+((1,1),(1,1))
+-}
+
+testBitSum :: Bool
+testBitSum = all (\(args, value) -> uncurry bitSum args == value)
+  [((Zero, Zero), (Zero, Zero)),
+   ((Zero, One), (One, Zero)),
+   ((One, Zero), (One, Zero)),
+   ((One, One), (One, One))]
+
+bitSum3 :: Bit -> Bit -> Bit -> (Bit, Bit)
+bitSum3 a b c = case bitSum a b of
+  (ab, ab') -> let (abc, abc') = bitSum ab c
+                   (abc'', _) = bitSum ab' abc'
+               in (abc, abc'')
+
+{- mapM_ (\x -> print (x, (uncurry3 bitSum3) x)) [(a, b, c) | a <- bits, b <- bits, c <- bits]
+=>
+((0,0,0),(0,0))
+((0,0,1),(1,0))
+((0,1,0),(1,0))
+((0,1,1),(1,1))
+((1,0,0),(1,0))
+((1,0,1),(1,1))
+((1,1,0),(1,1))
+((1,1,1),(1,1))
+-}
+
+testBitSum3 :: Bool
+testBitSum3 = all (\(args, value) -> uncurry3 bitSum3 args == value)
+  [((Zero, Zero, Zero), (Zero, Zero)),
+   ((Zero, Zero, One), (One, Zero)),
+   ((Zero, One, Zero), (One, Zero)),
+   ((Zero, One, One), (One, One)),
+   ((One, Zero, Zero), (One, Zero)),
+   ((One, Zero, One), (One, One)),
+   ((One, One, Zero), (One, One)),
+   ((One, One, One), (One, One))]
+
+-- add :: Z -> Z -> Z
+-- add (Z _ []) y = y
+-- add x (Z _ []) = x
+-- add (Z sx (x:xs)) (Z sy (y:ys)) =
+--   let rest = add (Z sx xs)
+--   in undefined
+
