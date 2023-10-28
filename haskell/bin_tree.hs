b08ccc1a07c2a2317288f1b12c4863a5de4f4050
anton0xf <anton0xf@gmail.com>
2023-10-28 21:08:28 +0300

    haskell/stepik: 4.5.5: Tree height and size - write tests

--- haskell/bin_tree.hs
+++ haskell/bin_tree.hs
@@ -11,12 +11,39 @@ data Tree a = Leaf a | Node (Tree a) (Tree a)
 
 data Tree a = Leaf a | Node (Tree a) (Tree a)
 
-height :: Tree a -> Int
 height (Leaf _) = 0
 height (Node a b) = 1 + max ha hb
   where ha = height a
         hb = height b
 
+testHeight :: Bool
+testHeight = and [
+    height (Leaf 1) == 0,
+    height t1 == 1,
+    height t2 == 2,
+    height t3 == 1,
+    height t4 == 3]
+  where
+    t1 = Node (Leaf 2) (Leaf 3)
+    t2 = Node (Leaf 1) t1
+    t3 = Node (Leaf 2) (Leaf 3)
+    t4 = Node t2 t3
+
 size :: Tree a -> Int
 size (Leaf _) = 1
 size (Node a b) = 1 + size a + size b
+
+testSize :: Bool
+testSize = and [
+    size (Leaf 1) == 1,
+    size t1 == 3,
+    size t2 == 5,
+    size t3 == 3,
+    size t4 == 9]
+  where
+    t1 = Node (Leaf 2) (Leaf 3)
+    t2 = Node (Leaf 1) t1
+    t3 = Node (Leaf 2) (Leaf 3)
+    t4 = Node t2 t3
+
+test = testHeight && testSize
