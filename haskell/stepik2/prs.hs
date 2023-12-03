0849a87ec40f48b0486c07be5820ca1b297e4106
anton0xf <anton0xf@gmail.com>
2023-12-03 15:32:23 +0300

    haskell/stepik2: 1.4.6. Prs is Applicative

--- haskell/stepik2/prs.hs
+++ haskell/stepik2/prs.hs
@@ -22,8 +22,29 @@ anyChr = Prs p where
   p "" = Nothing
   p (c : cs) = Just (c, cs)
 
-test :: Bool
-test = runPrs anyChr "ABC" == Just ('A',"BC")
+testFunctor :: Bool
+testFunctor = runPrs anyChr "ABC" == Just ('A',"BC")
   && isNothing (runPrs anyChr "")
   && runPrs (digitToInt <$> anyChr) "BCD" == Just (11,"CD")
 
+{- https://stepik.org/lesson/30425/step/6?unit=11042
+1.4.6. Аппликативный парсер своими руками
+
+Сделайте парсер аппликативным функтором с естественной для парсера семантикой -}
+
+instance Applicative Prs where
+  pure :: a -> Prs a
+  pure x = Prs $ \s -> Just (x, s)
+
+  (<*>) :: Prs (a -> b) -> Prs a -> Prs b
+  (Prs pf) <*> (Prs px) = Prs $ \s -> do
+    (f, s1) <- pf s
+    (x, s2) <- px s1
+    return (f x, s2)
+
+testApp :: Bool
+testApp = runPrs ((,,) <$> anyChr <*> anyChr <*> anyChr) "ABCDE" == Just (('A','B','C'),"DE")
+  && runPrs (anyChr *> anyChr) "ABCDE" == Just ('B',"CDE")
+
+test :: Bool
+test = testFunctor && testApp
