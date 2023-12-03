d55f42325d6dc2efc9782fda159bb7b211e308bd
anton0xf <anton0xf@gmail.com>
2023-12-03 18:50:45 +0300

    aoc2023: day1 - treuchet: wrong solution for part2

--- advent-of-code/aoc2023/day01_trebuchet/solution.hs
+++ advent-of-code/aoc2023/day01_trebuchet/solution.hs
@@ -1,6 +1,8 @@
 {- --- Day 1: Trebuchet?! --- https://adventofcode.com/2023/day/1 -}
 import Data.Function ((&))
 import Data.Char
+import Data.List
+import Data.Either
 import Text.Parsec
 import Text.Parsec.Char
 import System.IO
@@ -38,8 +40,8 @@ sumLines = sum . map (digitsToVal . strToDigits)
 testSumLines :: Bool
 testSumLines = (sumLines <$> parse parseInput "" testInput) == Right 142
 
-test :: Bool
-test = testParseInput && testStrToDigits && testSumLines
+test1 :: Bool
+test1 = testParseInput && testStrToDigits && testSumLines
 
 processInput :: IO ()
 processInput = do
@@ -51,4 +53,60 @@ processInput = do
 
 -- processInput == Right 54388
 
+spelledDigits :: [String]
+spelledDigits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
+getDigits :: Parsec [Char] u [Int]
+getDigits = (singleton . digitToInt <$> digit)
+  <|> choice (zipWith (\n s -> [n] <$ try (string s)) [1..] spelledDigits)
+  <|> [] <$ letter
+
+testGetDigits :: Bool
+testGetDigits = isLeft (parse getDigits "" "")
+  && parse getDigits "" "1a2" == Right [1]
+  && parse (concat <$> many1 getDigits) "" "1a2" == Right [1, 2]
+  && parse (concat <$> many1 getDigits) "" "1aone2" == Right [1, 1, 2]
+  && parse (concat <$> many1 getDigits) "" "ttwothree" == Right [2, 3]
+
+parseInput2 :: Parsec [Char] u [[Int]]
+parseInput2 = (concat <$> many getDigits) `sepBy` newline
+
+testInput2 :: String
+testInput2 = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n"
+  ++ "4nineeightseven2\nzoneight234\n7pqrstsixteen"
+
+testParseInput2 :: Bool
+testParseInput2 = parse parseInput2 "" testInput2
+    == Right [[2, 1, 9], [8, 2, 3], [1, 2, 3], [2, 1, 3, 4], [4, 9, 8, 7, 2],
+              [1, 8, 2, 3, 4], [7, 6]]
+  && parse parseInput2 "" "1\n" == Right [[1], []]
+
+digitsTails :: [Int] -> (Int, Int)
+digitsTails [] = (0, 0)
+digitsTails ds = (head ds, last ds)
+
+testDigitsTails :: Bool
+testDigitsTails = map (digitsToVal . digitsTails) dss == [29, 83, 13, 24, 42, 14, 76]
+  where (Right dss) = parse parseInput2 "" testInput2
+
+sumDss :: [[Int]] -> Integer
+sumDss = sum . map (digitsToVal . digitsTails)
+
+testSumDss :: Bool
+testSumDss = (sumDss <$> parse parseInput2 "" testInput2) == Right 281
+
+test2 :: Bool
+test2 = testGetDigits && testParseInput2 && testDigitsTails && testSumDss
+
+processInput2 :: IO ()
+processInput2 = do
+  let fname = "input.txt"
+  fh <- openFile fname ReadMode
+  input <- hGetContents fh
+  print (sumDss <$> parse parseInput2 fname input)
+  hClose fh
+
+-- processInput2 == Right 53519
+
+test :: Bool
+test = test1 && test2
