9cecafc10ffa9f104f4b7be1df7f1942ec6b9b7c
anton0xf <anton0xf@gmail.com>
2023-12-10 17:36:58 +0300

    haskell/cabal-example: run simple test

--- haskell/cabal-example/src/MyLib.hs
+++ haskell/cabal-example/src/MyLib.hs
@@ -1,4 +1,9 @@
-module MyLib (someFunc) where
+module MyLib (someFunc, fib) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 2) + fib (n - 1)
--- haskell/cabal-example/test/MyLibTest.hs
+++ haskell/cabal-example/test/MyLibTest.hs
@@ -1,4 +1,10 @@
 module Main (main) where
 
+import MyLib
+import System.Exit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  putStrLn "MyLibTest: run"
+  if take 5 (map fib [1..]) == [1, 1, 2, 3, 5]
+  then exitSuccess else exitFailure
