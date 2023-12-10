098c55b2383730e13ea66a900b765356f4af36d0
anton0xf <anton0xf@gmail.com>
2023-12-10 21:43:02 +0300

    haskell/cabal-example: run simple test using HUnit

--- haskell/cabal-example/test/MyLibTest.hs
+++ haskell/cabal-example/test/MyLibTest.hs
@@ -2,9 +2,16 @@ module Main (main) where
 
 import MyLib
 import System.Exit
+import Test.HUnit
+
+testFib = TestCase (assertEqual "first five fibs"
+                    [1, 1, 2, 3, 5]
+                    (take 5 (map fib [1..])))
+
+testSet = [testFib]
 
 main :: IO ()
 main = do
-  putStrLn "MyLibTest: run"
-  if take 5 (map fib [1..]) == [1, 1, 2, 3, 5]
-  then exitSuccess else exitFailure
+  counts <- runTestTT (test testSet)
+  if errors counts + failures counts == 0
+    then exitSuccess else exitFailure
