b4add99c7ed5027f4e5ca8ca1793d6b1faf383c2
anton0xf <anton0xf@gmail.com>
2023-12-11 18:27:23 +0300

    haskell/stepik2: 1.5.3. Cmps: examples

--- haskell/stepik2/cmps.hs
+++ haskell/stepik2/cmps.hs
@@ -6,3 +6,18 @@ infixr 9 |.|
 newtype (|.|) f g a = Cmps { getCmps :: f (g a) }
   deriving (Show, Eq)
 
+{- https://stepik.org/lesson/30426/step/3?unit=11043
+Населите допустимыми нерасходящимися выражениями следующие типы -}
+
+type A   = ((,) Integer |.| (,) Char) Bool
+type B t = ((,,) Bool (t -> t) |.| Either String) Int
+type C   = (|.|) ((->) Bool) ((->) Integer) Integer
+
+a :: A
+a = Cmps (1, ('a', True))
+
+b :: B t
+b = Cmps (True, id, Left "asdf")
+
+c :: C
+c  = Cmps $ const (+1)
