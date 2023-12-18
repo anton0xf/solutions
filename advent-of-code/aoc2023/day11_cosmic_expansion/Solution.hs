287cc73c49350accc34a2612366bcf0e4e4f0251
anton0xf <anton0xf@gmail.com>
2023-12-18 16:03:50 +0300

    aoc2023: Day 11: Cosmic Expansion: solve part 1

---
+++ advent-of-code/aoc2023/day11_cosmic_expansion/Solution.hs
@@ -0,0 +1,123 @@
+{- --- Day 11: Cosmic Expansion --- https://adventofcode.com/2023/day/11 -}
+module Solution where
+
+import Data.Maybe
+import Data.List hiding ((\\))
+import Data.Function ((&))
+import Data.Ord
+import Data.Bifunctor
+import System.IO
+import Text.Parsec hiding (space, spaces)
+
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+
+import Data.Set (Set, (\\))
+import qualified Data.Set as Set
+
+-- data
+
+type Loc = (Int, Int) -- (row, col), 0-based
+type Bounds = (Loc, Loc) -- (top left corner, bottom right corner) inclusive
+
+-- parsing
+
+tryParse :: Parsec String () a -> String -> a
+tryParse p = either undefined id . parse p ""
+
+emptyP :: Parsec String u ()
+emptyP = skipMany $ char '.'
+
+sourceLoc :: SourcePos -> Loc
+sourceLoc pos = (sourceLine pos - 1, sourceColumn pos - 1)
+
+starP :: Parsec String u Loc
+starP = (sourceLoc <$> getPosition) <* char '#'
+
+spaceMapP :: Parsec String u [Loc]
+spaceMapP = concat <$> endBy1 (emptyP *> endBy starP emptyP) newline
+
+parseIn :: String -> [Loc]
+parseIn = tryParse spaceMapP
+
+-- part 1
+
+bounds :: [Loc] -> Bounds
+bounds locs = let rows = map fst locs
+                  cols = map snd locs
+              in ((minimum rows, minimum cols),
+                  (maximum rows, maximum cols))
+
+-- return asc ordered list of empty rows' nums
+emptyRows :: Bounds -> [Loc] -> [Int]
+emptyRows ((minRow, _), (maxRow, _)) locs = let
+      rows = Set.fromList $ map fst locs
+  in filter (not . (`Set.member` rows)) [minRow .. maxRow]
+
+-- return asc ordered list of empty cols' nums
+emptyCols :: Bounds -> [Loc] -> [Int]
+emptyCols ((_, minCol), (_, maxCol)) locs = let
+      cols = Set.fromList $ map snd locs
+  in filter (not . (`Set.member` cols)) [minCol .. maxCol]
+
+-- asc ordered rows' nums -> locs -> new locs
+expandEmptyRows :: [Int] -> [Loc] -> [Loc]
+expandEmptyRows rows locs = foldr (map . incGtRows) locs rows
+  where incGtRows emptyRow loc@(row, col) = if row > emptyRow
+          then (row + 1, col) else loc
+
+-- asc ordered cols' nums -> locs -> new locs
+expandEmptyCols :: [Int] -> [Loc] -> [Loc]
+expandEmptyCols cols locs = foldr (map . incGtCols) locs cols
+  where incGtCols emptyCol loc@(row, col) = if col > emptyCol
+          then (row, col + 1) else loc
+
+expand :: Bounds -> [Loc] -> [Loc]
+expand bounds locs = let
+    rs = emptyRows bounds locs
+    cs = emptyCols bounds locs
+  in expandEmptyCols cs . expandEmptyRows rs $ locs
+
+expand' :: [Loc] -> [Loc]
+expand' locs = expand (bounds locs) locs
+
+manhattan :: Loc -> Loc -> Integer
+manhattan (r1, c1) (r2, c2) = fromIntegral $ abs (r2 - r1) + abs (c2 - c1)
+
+-- get all 2-combinations of list
+pairsOf :: [a] -> [(a, a)]
+pairsOf [] = []
+pairsOf (x : xs) = map (x,) xs ++ pairsOf xs
+
+allDists :: [Loc] -> [Integer]
+allDists = map (uncurry manhattan) . pairsOf
+
+solve1 :: String -> Integer
+solve1 = sum . allDists . expand' . parseIn
+
+solution :: (String -> Integer) -> IO ()
+solution solve = do
+  fh <- openFile "input.txt" ReadMode
+  input <- hGetContents fh
+  print $ solve input
+  hClose fh
+
+solution1 :: IO ()
+solution1 = solution solve1
+
+-- part 2
+
+solve2 :: String -> Integer
+solve2 _ = 0
+
+solution2 :: IO ()
+solution2 = solution solve2
+
+-- main
+
+main :: IO ()
+main = do
+  putStr "Part 1: "
+  solution1
+  putStr "Part 2: "
+  solution2
---
+++ advent-of-code/aoc2023/day11_cosmic_expansion/Test.hs
@@ -0,0 +1,100 @@
+module Test where
+
+import Solution hiding (main)
+import Test.HUnit
+import System.Exit
+import Text.Parsec hiding (space, spaces)
+import Data.Function ((&))
+import Data.List
+import Data.Bifunctor
+import Data.Tuple
+import Data.Maybe
+
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+
+import Data.Set (Set, (\\))
+import qualified Data.Set as Set
+
+inEx :: String
+inEx = "...#......\n"
+    ++ ".......#..\n"
+    ++ "#.........\n"
+    ++ "..........\n"
+    ++ "......#...\n"
+    ++ ".#........\n"
+    ++ ".........#\n"
+    ++ "..........\n"
+    ++ ".......#..\n"
+    ++ "#...#.....\n"
+
+locsEx :: [Loc]
+locsEx = [(0, 3), (1, 7), (2, 0), (4, 6), (5, 1), (6, 9), (8, 7), (9, 0), (9, 4)]
+
+boundsEx :: Bounds
+boundsEx = ((0, 0), (9, 9))
+
+emptyRowsEx :: [Int]
+emptyRowsEx = [3, 7]
+
+emptyColsEx :: [Int]
+emptyColsEx = [2, 5, 8]
+
+locsEREx :: [Loc]
+locsEREx = [(0, 3), (1, 7), (2, 0), (5, 6), (6, 1), (7, 9), (10, 7), (11, 0), (11, 4)]
+
+locsEEx :: [Loc]
+locsEEx = [(0, 4), (1, 9), (2, 0), (5, 8), (6, 1), (7, 12), (10, 9), (11, 0), (11, 5)]
+
+parseInTest :: Test
+parseInTest = "parseIn" ~: parseIn inEx ~?= locsEx
+
+boundsTest :: Test
+boundsTest = "bounds" ~: bounds locsEx ~?= boundsEx
+
+emptyRowsTest :: Test
+emptyRowsTest = "emptyRows" ~: emptyRows boundsEx locsEx ~?= emptyRowsEx
+
+emptyColsTest :: Test
+emptyColsTest = "emptyCols" ~: emptyCols boundsEx locsEx ~?= emptyColsEx
+
+expandEmptyRowsTest :: Test
+expandEmptyRowsTest = "expandEmptyRows" ~: expandEmptyRows emptyRowsEx locsEx ~?= locsEREx
+
+expandEmptyColsTest :: Test
+expandEmptyColsTest = "expandEmptyCols" ~: expandEmptyCols emptyColsEx locsEREx ~?= locsEEx
+
+expandTest :: Test
+expandTest = "expand" ~: expand boundsEx locsEx ~?= locsEEx
+
+manhattanTest :: Test
+manhattanTest = "manhattan" ~: manhattan (6, 1) (11, 5) ~?= 9
+
+pairsOfTest :: Test
+pairsOfTest = "pairsOf" ~: pairsOf [1, 2, 3] ~?= [(1, 2), (1, 3), (2, 3)]
+
+allDistsTest :: Test
+allDistsTest = "allDists" ~: allDists locsEEx
+  ~?= [6,6,9,9,15,15,15,12,10,5,13,9,9,19,14,11,5,17,17,9,14,8,6,6,14,9,12,12,6,9,6,16,11,10,5,5]
+
+solve1Test :: Test
+solve1Test = "solve1" ~: solve1 inEx ~?= 374
+
+tests1 :: Test
+tests1 = "part 1" ~: test [
+  parseInTest, boundsTest,
+  emptyRowsTest, emptyColsTest, expandEmptyRowsTest, expandEmptyColsTest, expandTest,
+  manhattanTest, pairsOfTest, allDistsTest, solve1Test]
+
+-- part 2
+
+tests2 :: Test
+tests2 = "part 2" ~: test [True ~? "stub"]
+
+-- main
+
+main :: IO ()
+main = do
+  counts <- runTestTT $ test [tests1, tests2]
+  if errors counts + failures counts == 0
+    then exitSuccess else exitFailure
