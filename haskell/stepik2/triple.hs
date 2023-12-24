37215be2e8226eda575e99fe8a8d929f0b9ae778
anton0xf <anton0xf@gmail.com>
2023-12-24 20:29:13 +0300

    haskell/stepik2: 2.2.11. Traversable Triple

--- haskell/stepik2/triple.hs
+++ haskell/stepik2/triple.hs
@@ -19,5 +19,30 @@ foldrTest = foldr (++) "!!" (Tr "ab" "cd" "efg") == "abcdefg!!"
 foldlTest :: Bool
 foldlTest = foldl (++) "!!" (Tr "ab" "cd" "efg") == "!!abcdefg"
 
+{- https://stepik.org/lesson/30428/step/11?unit=11045
+2.2.11. Класс типов Traversable
+Сделайте тип Triple представителем класса типов Traversable -}
+
+instance Functor Triple where
+  fmap :: (a -> b) -> Triple a -> Triple b
+  fmap f (Tr x y z) = Tr (f x) (f y) (f z)
+
+instance Applicative Triple where
+  pure :: a -> Triple a
+  pure x = Tr x x x
+  
+  (<*>) :: Triple (a -> b) -> Triple a -> Triple b
+  (Tr f1 f2 f3) <*> (Tr x1 x2 x3) = Tr (f1 x1) (f2 x2) (f3 x3)
+
+instance Traversable Triple where
+  sequenceA :: Applicative f => Triple (f a) -> f (Triple a)
+  sequenceA (Tr fx fy fz) = Tr <$> fx <*> fy <*> fz
+
+traversableTest :: Bool
+traversableTest = foldl (++) "!!" (Tr "ab" "cd" "efg") == "!!abcdefg"
+  && traverse (\x -> if x>10 then Right x else Left x) (Tr 12 14 16) == Right (Tr 12 14 16)
+  && traverse (\x -> if x>10 then Right x else Left x) (Tr 12 8 4) == Left 8
+  && sequenceA (Tr (Tr 1 2 3) (Tr 4 5 6) (Tr 7 8 9)) == Tr (Tr 1 4 7) (Tr 2 5 8) (Tr 3 6 9)
+
 test :: Bool
-test = foldrTest && foldlTest
+test = foldrTest && foldlTest && traversableTest
