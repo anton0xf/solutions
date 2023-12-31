d0f6b1f0937c3cdbcace9ab01feb7b21eb357a56
anton0xf <anton0xf@gmail.com>
2023-12-31 12:48:43 +0300

    haskell/stepik2: 2.3.2-3. Traversable laws borrowed from Functor

---
+++ haskell/stepik2/traverse.hs
@@ -0,0 +1,28 @@
+{- https://stepik.org/lesson/31555/step/2?unit=11808
+2.3.2. Законы и свойства класса Traversable -}
+
+import Data.List
+import Data.Functor.Identity
+import Data.Functor.Compose
+
+{- Functor Laws
+identity:    fmap id == id
+composition: fmap (g2 . g1) == fmap g2 . fmap g1
+-}
+
+{- Traversable Laws
+identity:    traverse Identity == Identity
+composition: traverse (Compose . fmap g1 . g2) == Compose . fmap (traverse g1) . traverse g2
+-}
+
+idTest :: Bool
+idTest = traverse Identity xs == Identity xs
+  where xs = [0..5]
+
+{- https://stepik.org/lesson/31555/step/3?unit=11808 -}
+compTest :: Bool
+compTest = traverse (Compose . fmap g1 . g2) xs == (Compose . fmap (traverse g1) . traverse g2) xs
+  where g1 = Just; g2 = singleton; xs = [0..5]
+
+test :: Bool
+test = idTest && compTest
