bb374e2fd55facfddf8069049606d8b0fa8b371c
anton0xf <anton0xf@gmail.com>
2024-01-20 12:58:10 +0300

    haskell/stepik2: 3.3.2-3. ReaderT usage example

--- haskell/stepik2/monad_reader_writer.hs
+++ haskell/stepik2/monad_reader_writer.hs
@@ -1,12 +1,30 @@
-{- https://stepik.org/lesson/31556/step/4?unit=11810
-3.3.4. Трансформеры монад -}
+{- https://stepik.org/lesson/31556/step/2?unit=11810
+3.3.2. Трансформеры монад -}
 
 import Data.Char
 import Control.Monad.Trans
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Writer
 
-{- Перепишите функцию logFirstAndRetSecond из предыдущего видео, -}
+secondElem :: Reader [String] String
+secondElem = asks (map toUpper . head . tail)
+
+strs :: [String]
+strs = ["abc", "defg", "qwer", "asdf"]
+
+secondElemTest :: Bool
+secondElemTest = runReader secondElem strs == "DEFG"
+
+logFirst :: [String] -> Writer String String
+logFirst xs = do
+  tell $ head xs
+  return $ map toUpper $ head $ tail xs
+
+logFirstTest :: Bool
+logFirstTest = runWriter (logFirst strs) == ("DEFG", "abc")
+
+{- https://stepik.org/lesson/31556/step/3?unit=11810
+3.3.3. Трансформеры монад -}
 
 logFirstAndRetSecond1 :: ReaderT [String] (Writer String) String
 logFirstAndRetSecond1 = do
@@ -15,13 +33,14 @@ logFirstAndRetSecond1 = do
   lift $ tell e1
   return e2
 
-strs :: [String]
-strs = ["abc", "defg", "qwer", "asdf"]
-
 logFirstAndRetSecond1Test :: Bool
 logFirstAndRetSecond1Test = runWriter (runReaderT logFirstAndRetSecond1 strs) == ("DEFG", "abc")
 
-{- используя трансформер WriterT из модуля Control.Monad.Trans.Writer
+{- https://stepik.org/lesson/31556/step/4?unit=11810
+3.3.4. Трансформеры монад -}
+
+{- Перепишите функцию logFirstAndRetSecond из предыдущего видео,
+используя трансформер WriterT из модуля Control.Monad.Trans.Writer
 библиотеки transformers, и монаду Reader в качестве базовой. -}
 
 logFirstAndRetSecond2 :: WriterT String (Reader [String]) String
@@ -69,6 +88,8 @@ plusAndMult2Test = (runWriter . mapM writer . runWriterT . plusAndMult2) 3
   == ([      3,        4,        5,        6,        0,        3,        6,        9],
       ["0 -> 3", "1 -> 4", "2 -> 5", "3 -> 6", "0 -> 0", "1 -> 3", "2 -> 6", "3 -> 9"])
 
+-- all tests
 test :: Bool
-test = logFirstAndRetSecond1Test && logFirstAndRetSecond2Test
+test = secondElemTest && logFirstTest
+  && logFirstAndRetSecond1Test && logFirstAndRetSecond2Test
   && plusAndMult0Test && plusAndMult1Test && plusAndMult2Test
