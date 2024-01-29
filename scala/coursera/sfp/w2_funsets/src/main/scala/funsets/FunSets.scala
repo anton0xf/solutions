c38388968be8fd74293ab2defa9a2051d536619f
anton0xf <anton0xf@gmail.com>
2024-01-29 11:42:35 +0300

    corsera/sfp/w2: solve Functional Sets assignment

--- scala/coursera/sfp/w2_funsets/src/main/scala/funsets/FunSets.scala
+++ scala/coursera/sfp/w2_funsets/src/main/scala/funsets/FunSets.scala
@@ -18,31 +18,35 @@ trait FunSets extends FunSetsInterface:
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): FunSet = ???
+  def singletonSet(elem: Int): FunSet = x => x == elem
 
+  /** Returns a set from `a` to `b` including `a` to `b` */
+  def rangeSet(a: Int, b: Int): FunSet = x => a <= x && x <= b
+
+  def fromSet(xs: Set[Int]): FunSet = x => xs.contains(x)
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: FunSet, t: FunSet): FunSet = ???
+  def union(s: FunSet, t: FunSet): FunSet = x => s(x) || t(x)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: FunSet, t: FunSet): FunSet = ???
+  def intersect(s: FunSet, t: FunSet): FunSet = x => s(x) && t(x)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: FunSet, t: FunSet): FunSet = ???
+  def diff(s: FunSet, t: FunSet): FunSet = x => s(x) && !t(x)
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: FunSet, p: Int => Boolean): FunSet = ???
+  def filter(s: FunSet, p: Int => Boolean): FunSet = intersect(s, p)
 
 
   /**
@@ -54,25 +58,36 @@ trait FunSets extends FunSetsInterface:
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
   def forall(s: FunSet, p: Int => Boolean): Boolean =
-    def iter(a: Int): Boolean =
-      if ??? then
-        ???
-      else if ??? then
-        ???
-      else
-        iter(???)
-    iter(???)
+    def iter(x: Int): Boolean =
+      if x > bound then true
+      else if s(x) && !p(x) then false
+      else iter(x + 1)
+    iter(-bound)
+
+  def neg(p: Int => Boolean)(x: Int): Boolean = !p(x)
+
+  def isNotEmpty(s: FunSet): Boolean =
+    def iter(x: Int): Boolean =
+      if x > bound then false
+      else if s(x) then true
+      else iter(x + 1)
+    iter(-bound)
+
+  def isEmpty(s: FunSet): Boolean = !isNotEmpty(s)
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: FunSet, p: Int => Boolean): Boolean = ???
+  def exists(s: FunSet, p: Int => Boolean): Boolean = !forall(s, neg(p))
+    // isNotEmpty(intersect(s, p))
+
+  def isEqual(s: FunSet, t: FunSet): Boolean = isEmpty(diff(s, t))
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: FunSet, f: Int => Int): FunSet = ???
+  def map(s: FunSet, f: Int => Int): FunSet = y => exists(s, x => y == f(x))
 
   /**
    * Displays the contents of a set
--- scala/coursera/sfp/w2_funsets/src/test/scala/funsets/FunSetSuite.scala
+++ scala/coursera/sfp/w2_funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -11,6 +11,9 @@ class FunSetSuite extends munit.FunSuite:
 
   test("contains is implemented") {
     assert(contains(x => true, 100))
+    assert(!contains(x => false, 100))
+    assert(contains(x => x == 0, 0))
+    assert(!contains(x => x == 0, 1))
   }
 
   /**
@@ -45,7 +48,7 @@ class FunSetSuite extends munit.FunSuite:
    * .ignore annotation.
    */
 
-    test("singleton set one contains one".ignore) {
+  test("singleton set one contains one") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -67,7 +70,102 @@ class FunSetSuite extends munit.FunSuite:
       assert(!contains(s, 3), "Union 3")
   }
 
+  test("intersect") {
+    new TestSets:
+      val s1_5 = rangeSet(1, 5)
+      val s3_10 = rangeSet(3, 10)
+      val s = intersect(s1_5, s3_10)
+      assert(!contains(s, 0))
+      assert(!contains(s, 1))
+      assert(!contains(s, 2))
+      assert(contains(s, 3))
+      assert(contains(s, 4))
+      assert(contains(s, 5))
+      assert(!contains(s, 6))
+      assert(!contains(s, 10))
+      assert(!contains(s, 11))
+  }
 
+  test("diff [1..3] and [2..5]") {
+    val s1_3 = rangeSet(1, 3)
+    val s2_5 = rangeSet(2, 5)
+    val s = diff(s1_3, s2_5)
+    assert(!s(0))
+    assert(s(1))
+    assert(!s(2))
+    assert(!s(3))
+    assert(!s(4))
+    assert(!s(5))
+    assert(!s(6))
+  }
+
+  test("diff") {
+    new TestSets:
+      assert(isEmpty(diff(s1, s1)))
+      assert(isEqual(diff(s1, s2), fromSet(Set(1, 2))))
+  }
+
+  test("diff of {1,3,4,5,7,1000} and {1,2,3,4}") {
+    val s = diff(fromSet(Set(1, 3, 4, 5, 7, 1000)), fromSet(Set(1, 2, 3, 4)))
+    assert(isEqual(s, fromSet(Set(2, 5, 7, 1000))))
+  }
+
+  test("filter") {
+    val s1_5 = rangeSet(1, 5)
+    val s = filter(s1_5, x => x % 2 == 0)
+    assert(!s(0))
+    assert(!s(1))
+    assert(s(2))
+    assert(!s(3))
+    assert(s(4))
+    assert(!s(5))
+    assert(!s(6))
+  }
+
+  test("forall") {
+    new TestSets:
+      val s1_5 = rangeSet(1, 5)
+      assert(forall(s1, s1))
+      assert(forall(s2, s2))
+      assert(forall(s3, s3))
+      assert(forall(s1_5, s1_5))
+  }
+
+  test("exists") {
+    assert(!exists(rangeSet(1, 5), x => x > 6))
+    assert(exists(rangeSet(1, 5), x => x % 2 == 0))
+  }
+
+  test("isNotEmpty") {
+    assert(isNotEmpty(singletonSet(1)))
+    assert(!isNotEmpty(_ => false))
+  }
+
+  test("isEmpty") {
+    new TestSets:
+      assert(!isEmpty(singletonSet(1)))
+      assert(isEmpty(_ => false))
+      assert(isEmpty(diff(s1, s1)))
+      assert(isEmpty(diff(singletonSet(6), singletonSet(6))))
+  }
+
+  test("eq") {
+    assert(!isEqual(singletonSet(5), singletonSet(6)))
+    assert(isEqual(singletonSet(6), singletonSet(6)))
+    assert(!isEqual(rangeSet(1, 6), rangeSet(2, 6)))
+    assert(!isEqual(rangeSet(1, 6), rangeSet(1, 5)))
+    assert(isEqual(rangeSet(1, 6), rangeSet(1, 6)))
+  }
+
+  test("map") {
+    val s = map(singletonSet(1), x => x + 5)
+    assert(!s(0))
+    assert(!s(5))
+    assert(s(6))
+    assert(!s(7))
+    assert(!s(100))
+    assert(isEqual(s, singletonSet(6)))
+  }
 
   import scala.concurrent.duration.*
   override val munitTimeout = 10.seconds
