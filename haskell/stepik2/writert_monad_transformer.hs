1dedafc1550c248b21d62d4b10686d02b56669eb
anton0xf <anton0xf@gmail.com>
2024-01-31 06:38:56 +0300

    haskell/stepik2: 4.1.6. WriterT - MonadFail

--- haskell/stepik2/writert_monad_transformer.hs
+++ haskell/stepik2/writert_monad_transformer.hs
@@ -1,6 +1,9 @@
 import Data.Bifunctor
 import Data.Tuple
 import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Fail
 
 {- https://stepik.org/lesson/38578/step/2?unit=20503
 4.1.2. Трансформер WriterT -}
@@ -39,6 +42,8 @@ apTest :: Bool
 apTest = runWriterT (WriterT [((+1), "+1"), ((^2), "^2")] <*> WriterT [(3, "A"), (4, "B")])
   == [(4, "+1A"), (5, "+1B"), (9, "^2A"), (16, "^2B")]
 
+{- https://stepik.org/lesson/38578/step/6?unit=20503
+4.1.6. Трансформер WriterT -}
 instance (Monoid w, Monad m) => Monad (WriterT w m) where
   (>>=) :: WriterT w m a -> (a -> WriterT w m b) -> WriterT w m b
   (WriterT wx) >>= k = WriterT $ do
@@ -51,6 +56,27 @@ bindTest = runWriterT (WriterT [(3, "A"), (4, "B")] >>= k)
       == [(4, "A3+1"), (6, "A3*2"), (5, "B4+1"), (8, "B4*2")]
   where k x = WriterT [(x+1, show x ++ "+1"), (x*2, show x ++ "*2")]
 
+doTest :: Bool
+doTest = runWriterT (do {x <- writer [(3, "3"), (4, "4")];
+                         writer [(x+1, "+1"), (x*2, "*2")]})
+  == [(4, "3+1"), (6, "3*2"), (5, "4+1"), (8, "4*2")]
+
+instance Monoid w => MonadTrans (WriterT w) where
+  lift :: Monad m => m a -> WriterT w m a
+  -- lift m = WriterT $ (, mempty) <$> m
+  lift = WriterT . ((, mempty) <$>)
+
+instance (Monoid w, MonadFail m) => MonadFail (WriterT w m) where
+  fail :: String -> WriterT w m a
+  -- fail = lift . fail
+  fail = WriterT . fail
+
+failTest :: Bool
+failTest = runWriterT (do {x <- writer [(3, "3"), (4, "4")];
+                           when (x /= 3) $ fail "msg";
+                           writer [(x+1, "+1"), (x*2, "*2")]})
+  == [(4, "3+1"), (6, "3*2")]
+
 -- run all test
 test :: Bool
-test = runWriterTTest && fmapTest && apTest && bindTest
+test = runWriterTTest && fmapTest && apTest && bindTest && doTest && failTest
