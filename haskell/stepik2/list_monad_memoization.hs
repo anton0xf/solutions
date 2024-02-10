6e3bacba63706f930f1565c890aaa07eb8875243
anton0xf <anton0xf@gmail.com>
2024-02-10 11:01:33 +0300

    haskell/stepik2: 4.1.6. buggy memoized list monad example

--- haskell/stepik2/list_monad_memoization.hs
+++ haskell/stepik2/list_monad_memoization.hs
@@ -15,11 +15,75 @@ from discussion at 4.1.6. Трансформер WriterT -}
 import Data.Map (Map)
 import Data.Map qualified as Map
 
--- list -> Map (sum -> count)
-jumps :: [Int] -> Map Int Int
+import Debug.Trace
+import Control.Monad.Identity
+import Control.Monad.State
+
+traceWith :: (a -> String) -> a -> a
+traceWith f x = trace (f x) x
+
+drops :: Int -> [Int] -> [[Int]]
+drops _ [] = [[]]
+drops 0 xs = [xs]
+drops n xs@(_ : xs') = xs : drops (n-1) xs'
+
+dropsTest :: Bool
+dropsTest = drops 0 [1, 2, 3] == [[1, 2, 3]]
+  && drops 1 [1, 2, 3] == [[1, 2, 3], [2, 3]]
+  && drops 2 [1, 2, 3] == [[1, 2, 3], [2, 3], [3]]
+  && drops 3 [1, 2, 3] == [[1, 2, 3], [2, 3], [3], []]
+  && drops 4 [1, 2, 3] == [[1, 2, 3], [2, 3], [3], []]
+
+type SumsMap = Map Int Int -- sum -> count
+
+jumps :: [Int] -> SumsMap
 jumps [] = Map.singleton 0 1
 jumps (x : xs) = Map.fromListWith (+) $ do
-     j <- [0 .. (x-1)]
-     (sum, count) <- Map.toList $ jumps $ drop j xs
+     xs' <- drops (x-1) xs
+     (sum, count) <- Map.toList $ jumps xs'
      return (sum + x, count)
 
+jumpsM :: Monad m => (Int -> [Int] -> m SumsMap) -> Int -> [Int] -> m SumsMap
+jumpsM _ _ [] = return $ Map.singleton 0 1
+jumpsM rec n (x : xs) =
+  let callStr = ("jumpsM(pos=" ++ show n
+                 ++ ", xs=(" ++ show x ++ " : " ++ show xs ++ "))")
+  in trace callStr $ do
+    -- TODO: all rec calls have the same pos and different drops
+    sums <- traverse (fmap Map.toList . rec (n+1))
+        $ traceWith (\ds -> "drops(pos=" ++ show n ++ "): " ++ show ds) (drops (x-1) xs)
+    let res = Map.fromListWith (+)
+                [(sum + x, count) | (sum, count) <- concat
+                    (trace ("sums (pos=" ++ show n ++ ") = " ++ show sums) sums)]
+    return $ trace (callStr ++ " = " ++ show res) res
+
+type MemMap = Map Int SumsMap -- pos -> sums map
+type MemState = State MemMap SumsMap
+
+jumpsMem :: [Int] -> (SumsMap, MemMap)
+jumpsMem xs = runState (fix (mem . jumpsM) 0 xs) Map.empty
+  where mem :: (Int -> [Int] -> MemState) -> Int -> [Int] -> MemState
+        mem rec pos xs = do
+                  m :: MemMap <- get
+                  case trace ("lookup " ++ show pos ++ " in " ++ show m) (Map.lookup pos m) of
+                  -- case Map.lookup pos m of
+                    Just sums -> return sums
+                    Nothing -> do
+                      sums <- rec pos xs
+                      modify $ Map.insert pos sums
+                      return sums
+
+jumpsTest :: ([Int] -> Map Int Int) -> Bool
+jumpsTest jumps = jumps [1, 1] == Map.fromList [(2, 1)]
+  && jumps [1, 1, 1] == Map.fromList [(3, 1)]
+  && jumps [2] == Map.singleton 2 1
+  && jumps [2, 1] == Map.fromList [(2, 1), (3, 1)]
+  && jumps [2, 2] == Map.fromList [(2, 1), (4, 1)]
+  && jumps [2, 2, 2] == Map.fromList [(6, 1), (4, 2)]
+  && jumps [2, 2, 2, 2] == Map.fromList [(4, 1), (6, 3), (8, 1)]
+  && jumps [3, 2, 2] == Map.fromList [(3, 1), (5, 2), (7, 1)]
+
+test :: Bool
+test = dropsTest && jumpsTest jumps
+  && jumpsTest (runIdentity . fix jumpsM 0)
+  && jumpsTest (fst . jumpsMem)
