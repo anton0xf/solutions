dc670e3e127a34a7e66d3962b639c86c5dc2f040
anton0xf <anton0xf@gmail.com>
2024-02-17 20:51:07 +0300

    haskell/stepik2: 4.5.3. tryRead treeSum

--- haskell/stepik2/try_read_monad_error.hs
+++ haskell/stepik2/try_read_monad_error.hs
@@ -1,7 +1,9 @@
 {- https://stepik.org/lesson/45331/step/2?unit=23645
 4.5.2. Задачи на трансформеры -}
 
+import Data.Monoid
 import Text.Read
+import Control.Monad.Writer
 import Control.Monad.Except
 
 {- Функция tryRead обладает единственным эффектом: в случае ошибки она должна прерывать
@@ -34,5 +36,38 @@ tryReadTest = runExcept (tryRead "5" :: ReadExcept Int) == Right 5
   && runExcept (tryRead "" :: ReadExcept (Bool, ())) == Left EmptyInput
   && runExcept (tryRead "wrong" :: ReadExcept (Bool, ())) == Left (NoParse "wrong")
 
+{- https://stepik.org/lesson/45331/step/3?unit=23645
+4.5.3. Задачи на трансформеры
+
+В очередной раз у вас есть дерево строковых представлений чисел и функция tryRead -}
+
+data Tree a = Leaf a | Fork (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+{- Просуммируйте числа в дереве, а если хотя бы одно прочитать не удалось, верните ошибку:  -}
+
+treeSumTest :: Bool
+treeSumTest = treeSum (Fork (Fork (Leaf "1") "2" (Leaf "oops")) "15" (Leaf "16"))
+      == Left (NoParse "oops")
+  && treeSum (Fork (Fork (Leaf "1") "2" (Leaf "0")) "15" (Leaf "16")) == Right 34
+
+treeSum :: Tree String -> Either ReadError Integer
+treeSum = toEither . runWriter . runExceptT . iter
+  where
+    readAndTell :: String -> ExceptT ReadError (Writer (Sum Integer)) ()
+    readAndTell s = tryRead s >>= tell . Sum
+
+    iter :: Tree String -> ExceptT ReadError (Writer (Sum Integer)) ()
+    iter (Leaf s) = readAndTell s
+    iter (Fork t1 s t2) = do
+      iter t1
+      n <- tryRead s
+      tell $ Sum n
+      iter t2
+
+    toEither :: (Either ReadError (), Sum Integer) -> Either ReadError Integer
+    toEither (Left e, _) = Left e
+    toEither (_, n) = Right $ getSum n
+
 test :: Bool
-test = tryReadTest
+test = tryReadTest && treeSumTest
