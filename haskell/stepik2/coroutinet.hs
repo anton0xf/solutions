00f6c57a00f8f58f26c4fa27313f5d5d0128c0ba
anton0xf <anton0xf@gmail.com>
2024-02-29 13:27:18 +0300

    haskell/stepik2: 4.5.6. CoroutineT: solve

--- haskell/stepik2/coroutinet.hs
+++ haskell/stepik2/coroutinet.hs
@@ -2,7 +2,9 @@
 4.5.6. Задачи на трансформеры -}
 
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
+import Data.Bifunctor
 import Control.Monad
+import Control.Monad.Identity
 import Control.Monad.State
 import Control.Monad.Writer
 import Data.Foldable
@@ -67,7 +69,7 @@ test2 :: Bool
 test2 = execWriter (runCoroutines coroutine3 coroutine4) == "1ab2cd"
 
 -- solution
-newtype CoroutineT m a = CoroutineT { runCoroutineT :: (a -> m ()) -> m () }
+newtype CoroutineT m a = CoroutineT { runCoroutineT :: m (Either (CoroutineT m a) a) }
 
 instance Monad m => Functor (CoroutineT m) where
   fmap :: (a -> b) -> CoroutineT m a -> CoroutineT m b
@@ -75,34 +77,52 @@ instance Monad m => Functor (CoroutineT m) where
 
 instance Monad m => Applicative (CoroutineT m) where
   pure :: a -> CoroutineT m a
-  pure x = CoroutineT $ \c -> c x
+  pure = CoroutineT . return . Right
 
   (<*>) :: CoroutineT m (a -> b) -> CoroutineT m a -> CoroutineT m b
   (<*>) = ap
 
 instance Monad m => Monad (CoroutineT m) where
   (>>=) :: CoroutineT m a -> (a -> CoroutineT m b) -> CoroutineT m b
-  (CoroutineT cx) >>= k = CoroutineT $ \c -> cx (\x -> runCoroutineT (k x) c)
+  (CoroutineT cx) >>= k = CoroutineT
+    $ cx >>= either (return . Left . (>>= k)) (runCoroutineT . k)
+  -- cx >>= k = CoroutineT $ do
+  --   ex <- runCoroutineT cx
+  --   case ex of
+  --     Right x -> runCoroutineT $ k x
+  --     Left cx' -> return $ Left $ cx' >>= k
 
 type LogCoroutine = CoroutineT (Writer [String])
 
-evalCorEx :: Show a => LogCoroutine a -> ((), [String])
-evalCorEx m = runWriter (runCoroutineT m c)
-  where c x = do { tell [show x]; return () }
+evalCorEx :: Show a => LogCoroutine a -> (a, [String])
+-- evalCorEx m = eval $ runWriter (runCoroutineT m)
+--   where eval (Right x, log) = (x, log)
+--         eval (Left m', log1) = let (x, log2) = evalCorEx m'
+--                                in (x, log1 ++ log2)
+evalCorEx = runWriter . runOneCoroutine
 
-coroutineEq :: Show a => LogCoroutine a -> LogCoroutine a -> Bool
+coroutineEq :: (Show a, Eq a) => LogCoroutine a -> LogCoroutine a -> Bool
 coroutineEq m1 m2 = evalCorEx m1 == evalCorEx m2
 
-kleisliEx :: a -> CoroutineT m a
-kleisliEx x = CoroutineT $ \c -> c x
+kleisliEx :: Show a => a -> LogCoroutine a
+kleisliEx x = do { tell [show x]; return x }
+  -- CoroutineT $ WriterT $ Identity (Right x, [show x])
 
-kleisliEx2 :: Enum a => a -> CoroutineT m a
-kleisliEx2 x = CoroutineT $ \c -> c (succ x)
+kleisliYEx :: (Enum a, Show a) => a -> LogCoroutine a
+kleisliYEx x = do
+  tell [show x]
+  yield
+  let x' = succ x
+  tell [show x']
+  return x'
 
-kleisliEx3 :: Enum a => a -> CoroutineT m a
-kleisliEx3 x = CoroutineT $ \c -> c (succ $ succ x)
+kleisliEx2 :: (Enum a, Show a) => a -> LogCoroutine a
+kleisliEx2 x = do { tell [show x]; return $ succ x }
 
-leftIdLaw :: Show a => a -> (a -> LogCoroutine a) -> Bool
+kleisliEx3 :: (Enum a, Show a) => a -> LogCoroutine a
+kleisliEx3 x = do { tell [show x]; return $ succ $ succ x }
+
+leftIdLaw :: (Show a, Eq a) => a -> (a -> LogCoroutine a) -> Bool
 leftIdLaw x k = coroutineEq (return x >>= k) (k x)
 
 {- HLINT ignore "Monad law, left identity" -}
@@ -110,34 +130,54 @@ leftIdLawTest :: Bool
 leftIdLawTest = leftIdLaw 'a' kleisliEx
 
 {- HLINT ignore "Monad law, right identity" -}
-rightIdLaw :: Show a => CoroutineT (Writer [String]) a -> Bool
+rightIdLaw :: (Show a, Eq a) => CoroutineT (Writer [String]) a -> Bool
 rightIdLaw m = coroutineEq (m >>= return)  m
 
 rightIdLawTest :: Bool
 rightIdLawTest = rightIdLaw (kleisliEx 'a')
+  && rightIdLaw (kleisliYEx 'a')
 
-assocLaw :: Show c => LogCoroutine a -> (a -> LogCoroutine b) -> (b -> LogCoroutine c) -> Bool
+assocLaw :: (Show c, Eq c)
+  => LogCoroutine a -> (a -> LogCoroutine b) -> (b -> LogCoroutine c) -> Bool
 assocLaw m k h = coroutineEq (m >>= (k >=> h)) ((m >>= k) >>= h)
 
 assocLawTest :: Bool
 assocLawTest = assocLaw (kleisliEx 'a') kleisliEx2 kleisliEx3
+  && assocLaw (kleisliYEx 'a') kleisliEx2 kleisliEx3
+  && assocLaw (kleisliEx 'a') kleisliYEx kleisliEx3
+  && assocLaw (kleisliEx 'a') kleisliEx2 kleisliYEx
 
 instance MonadWriter w m => MonadWriter w (CoroutineT m) where
   tell :: w -> CoroutineT m ()
-  tell = undefined
+  tell log = CoroutineT $ writer (Right (), log)
   
   listen :: CoroutineT m a -> CoroutineT m (a, w)
-  listen = undefined
+  listen (CoroutineT cx) = undefined
+  -- CoroutineT $ do
+  --   (ec, log) <- listen cx
+  --   case ec of
+  --     Right x -> return $ Right (x, log)
+  --     Left c -> do { tell log; return $ Left $ listen c }
 
   pass :: CoroutineT m (a, w -> w) -> CoroutineT m a
   pass = undefined
 
 runCoroutines :: Monad m => CoroutineT m () -> CoroutineT m () -> m ()
-runCoroutines (CoroutineT c1) (CoroutineT c2)
-  = c1 $ \cont1 -> _
+runCoroutines c1 c2 = do
+  e1 <- runCoroutineT c1
+  case e1 of
+    Right _ -> runOneCoroutine c2
+    Left e1' -> runCoroutines c2 e1'
+
+runOneCoroutine :: Monad m => CoroutineT m a -> m a
+runOneCoroutine c = do
+  e <- runCoroutineT c
+  case e of
+    Right x -> return x
+    Left c' -> runOneCoroutine c'
 
 yield :: Monad m => CoroutineT m ()
-yield = undefined
+yield = CoroutineT $ return $ Left $ return ()
 
 -- tests
 lawsTest :: Bool
