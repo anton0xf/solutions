2df0f4318beceafb09e49d24165cf7e3bf8b2576
anton0xf <anton0xf@gmail.com>
2024-03-14 13:02:37 +0300

    plotter: AI: fix problems with empty lines

--- other/plotter/encoder.py
+++ other/plotter/encoder.py
@@ -1,46 +1,41 @@
 # coding=utf-8
+import sys
+
+
 def encode_ascii_image(image):
     # Инициализация начальных значений
     x_pos, y_pos = 0, 0
     commands = []
+    height = len(image)
+    width = max(len(row) for row in image)
 
     for row in image:
-        for col, symbol in enumerate(row):
-            # Если текущий символ не пробел, то добавляем команды для движения и печати
-            if symbol != " ":
-                # Добавление команды перемещения пера вправо, если нужно
+        for col, symbol in enumerate(row.ljust(width)):
+            # Если символ не пробел, создаем команду для перемещения и печати
+            if symbol != ' ':
                 move_right = col - x_pos
                 if move_right > 0:
-                    commands.append("R{}".format(move_right))
-                # Добавление команды печати символа
-                commands.append("P{}".format(symbol))
+                    commands.append('R{}'.format(move_right))
+                commands.append('P{}'.format(symbol))
                 x_pos = col
-        # Переход на новую строку, если не последняя строка
-        if y_pos < len(image) - 1:
-            move_down = 1
-            move_left = x_pos
-            # Перемещаем перо вниз на одну позицию
-            if move_down > 0:
-                commands.append("D{}".format(move_down))
-            # Перемещение пера в начало следующей строки
-            if move_left > 0:
-                commands.append("L{}".format(move_left))
+
+        # Если это не последняя строка, переходим на новую строку
+        if y_pos < height - 1:
+            commands.append('D1')
+            if x_pos > 0:
+                commands.append('L{}'.format(x_pos))
             x_pos = 0
             y_pos += 1
 
-    # Вывод расчитанных размеров экрана и команд
-    height = len(image)
-    width = max(len(row) for row in image)
-    print(height)
-    print(width)
-    print("".join(commands))
+    # Возвращаем результат: размеры и команды
+    return height, width, ''.join(commands)
 
-# Считывание изображения до ввода пустой строки
-image = []
-while True:
-    line = input()
-    if line == "":
-        break
-    image.append(line)
 
-encode_ascii_image(image)
+# Чтение изображения из стандартного входа до EOF
+image_input = sys.stdin.readlines()
+image = [line.rstrip('\n') for line in image_input]
+
+height, width, command_string = encode_ascii_image(image)
+print(height)
+print(width)
+print(command_string)
