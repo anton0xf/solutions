6cfb1c05419743a1e6525cb4ce60079cc3a513fa
anton0xf <anton0xf@gmail.com>
2024-04-09 17:50:30 +0300

    stepik/scala/niznik: 2.3.3: minus

---
+++ scala/stepik/by-niznik/src/main/scala/unit2/lesson3/Minus.scala
@@ -0,0 +1,10 @@
+package unit2.lesson3
+// step 3: https://stepik.org/lesson/106491/step/3?unit=81017
+
+import scala.io.StdIn
+
+object Minus extends App {
+  val a = StdIn.readInt()
+  val b = StdIn.readInt()
+  println(a - b)
+}
