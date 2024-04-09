a109ef5d51224297ddf075e88bc0ceebe036a62a
anton0xf <anton0xf@gmail.com>
2024-04-09 17:57:13 +0300

    stepik/scala/niznik: 2.3.4: product 3 numbers

---
+++ scala/stepik/by-niznik/src/main/scala/unit2/lesson3/Product.scala
@@ -0,0 +1,14 @@
+package unit2.lesson3
+
+// step 4: https://stepik.org/lesson/106491/step/4?unit=81017
+
+import scala.io.StdIn
+
+object Product extends App {
+  {
+    val a = StdIn.readInt()
+    val b = StdIn.readInt()
+    val c = StdIn.readInt()
+    println(a * b * c)
+  }
+}
