43417039a0f5215b8d7b1f1791fa139937d0b55d
anton0xf <anton0xf@gmail.com>
2024-04-20 21:58:05 +0300

    stepik/scala/niznik: 5.6.7. divide quotients with errors - simplify (and add tests)

--- scala/stepik/by-niznik/src/main/scala/unit5/lesson6/EitherDivide.scala
+++ scala/stepik/by-niznik/src/main/scala/unit5/lesson6/EitherDivide.scala
@@ -17,27 +17,24 @@ package unit5.lesson6
 import java.lang.Math.abs
 
 object EitherDivide {
-  def divide(p: (Int, Int))(q: (Int, Int)): Either[String, (Int, Int)] =
-    checkInput(p)
-      .flatMap { case (pn: Int, pd: Int) =>
-        checkInput(q)
-          .flatMap { case (qn: Int, qd: Int) =>
-            if (qn == 0) Left("Zero divisor")
-            else Right((pn * qd, pd * qn))
-          }
-      }
-      .flatMap(r => if (!isRight(r)) Left("Improper result") else Right(r))
+  def divide(p: (Int, Int))(q: (Int, Int)): Either[String, (Int, Int)] = {
+    (for {
+      _ <- checkInput(p)
+      _ <- checkInput(q).filterOrElse(_._1 != 0, "Zero divisor")
+    } yield (p._1 * q._2, p._2 * q._1))
+      .filterOrElse(isRight, "Improper result")
       .map(simplify)
+  }
 
   def checkInput(p: (Int, Int)): Either[String, (Int, Int)] =
-    if (p._2 == 0) Left("Zero divisor")
-    else if (!isRight(p)) Left("Invalid input")
-    else Right(p)
+    Right(p)
+      .filterOrElse(_._2 != 0, "Zero divisor")
+      .filterOrElse(isRight, "Invalid input")
 
-  def isRight(p: (Int, Int)) = abs(p._1) < abs(p._2)
+  def isRight(p: (Int, Int)): Boolean = abs(p._1) < abs(p._2)
 
   def simplify(p: (Int, Int)): (Int, Int) = {
-    val d = BigInt(p._1).gcd(BigInt(p._2)).intValue
+    val d = BigInt(p._1).gcd(p._2).intValue
     (p._1 / d, p._2 / d)
   }
 }
--- scala/stepik/by-niznik/src/test/scala/unit5/lesson6/EitherDivideSpec.scala
+++ scala/stepik/by-niznik/src/test/scala/unit5/lesson6/EitherDivideSpec.scala
@@ -7,5 +7,23 @@ class EitherDivideSpec extends FunSuite {
 
   test("divide") {
     assertEquals(divide((1, 10))((2, 10)), Right((1,2)))
+
+    assertEquals(divide(1, 0)(1, 2), Left("Zero divisor"))
+    assertEquals(divide(1, 2)(1, 0), Left("Zero divisor"))
+    assertEquals(divide(1, 2)(0, 2), Left("Zero divisor"))
+
+    assertEquals(divide(3, 4)(1, 10), Left("Improper result"))
+    assertEquals(divide(1, 2)(1, 2), Left("Improper result"))
+
+    assertEquals(divide(2, 1)(1, 7), Left("Invalid input"))
+    assertEquals(divide(1, 2)(7, 1), Left("Invalid input"))
+    assertEquals(divide(1, 1)(2, 2), Left("Invalid input"))
+    assertEquals(divide(2, 1)(3, 1), Left("Invalid input"))
+
+    assertEquals(divide(1, 2)(2, 3), Right((3,4)))
+    assertEquals(divide(-1, 2)(2, 3), Right((-3,4)))
+    assertEquals(divide(1, -2)(2, 3), Right((3, -4)))
+    assertEquals(divide(1, 2)(-2, 3), Right((3, -4)))
+    assertEquals(divide(1, 2)(2, -3), Right((-3, 4)))
   }
 }
