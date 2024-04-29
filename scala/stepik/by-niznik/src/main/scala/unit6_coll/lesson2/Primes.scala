fdab24cda4624e26b00df79f577bf01aeb8565b4
anton0xf <anton0xf@gmail.com>
2024-04-29 16:07:34 +0300

    stepik/scala/niznik: 6.2.4. primes - use lazy cons

--- scala/stepik/by-niznik/src/main/scala/unit6_coll/lesson2/Primes.scala
+++ scala/stepik/by-niznik/src/main/scala/unit6_coll/lesson2/Primes.scala
@@ -4,5 +4,5 @@ package unit6_coll.lesson2
 object Primes {
   def isPrime(n: Int): Boolean = LazyList.from(2)
     .takeWhile(p => p * p <= n).forall(n % _ != 0)
-  lazy val list: LazyList[Int] = LazyList.from(2).filter(isPrime)
+  lazy val list: LazyList[Int] = 2 #:: LazyList.from(3, 2).filter(isPrime)
 }
