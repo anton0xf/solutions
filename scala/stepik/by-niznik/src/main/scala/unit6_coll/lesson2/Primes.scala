23a4cfe5cdbe5dd61186a98e885fbc0587ff09b0
anton0xf <anton0xf@gmail.com>
2024-04-29 16:21:27 +0300

    stepik/scala/niznik: 6.2.4. primes - recursive use of primes list

--- scala/stepik/by-niznik/src/main/scala/unit6_coll/lesson2/Primes.scala
+++ scala/stepik/by-niznik/src/main/scala/unit6_coll/lesson2/Primes.scala
@@ -2,7 +2,7 @@ package unit6_coll.lesson2
 // https://stepik.org/lesson/106520/step/4?unit=81046
 
 object Primes {
-  def isPrime(n: Long): Boolean = LazyList.iterate(2L)(_ + 1L)
+  def isPrime(n: Long): Boolean = list
     .takeWhile(p => p * p <= n).forall(n % _ != 0)
   lazy val list: LazyList[Long] = 2 #:: LazyList.iterate(3L)(_ + 2).filter(isPrime)
 }
