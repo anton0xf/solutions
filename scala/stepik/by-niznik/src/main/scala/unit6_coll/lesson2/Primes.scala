72be7dddd0069cd03944f5c89465710e46214891
anton0xf <anton0xf@gmail.com>
2024-04-29 16:13:22 +0300

    stepik/scala/niznik: 6.2.4. primes - long

--- scala/stepik/by-niznik/src/main/scala/unit6_coll/lesson2/Primes.scala
+++ scala/stepik/by-niznik/src/main/scala/unit6_coll/lesson2/Primes.scala
@@ -2,7 +2,7 @@ package unit6_coll.lesson2
 // https://stepik.org/lesson/106520/step/4?unit=81046
 
 object Primes {
-  def isPrime(n: Int): Boolean = LazyList.from(2)
+  def isPrime(n: Long): Boolean = LazyList.iterate(2L)(_ + 1L)
     .takeWhile(p => p * p <= n).forall(n % _ != 0)
-  lazy val list: LazyList[Int] = 2 #:: LazyList.from(3, 2).filter(isPrime)
+  lazy val list: LazyList[Long] = 2 #:: LazyList.iterate(3L)(_ + 2).filter(isPrime)
 }
