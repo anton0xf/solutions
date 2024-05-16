758f1663913647be5f033ca0ab881d1173d572d2
anton0xf <anton0xf@gmail.com>
2024-05-16 18:05:16 +0300

    stepik/scala/niznik: 8.1. Dict example - set parametrize map result item type

--- scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson1_gen/DictEx.scala
+++ scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson1_gen/DictEx.scala
@@ -7,7 +7,7 @@ trait DictItem[K] {
 
 case class DictEx[K, I <: DictItem[K]](items: List[I]) {
   def head: I = items.head
-  def map(f: String => String): DictEx[K, ADictItem[K]] =
+  def map[J >: ADictItem[K] <: DictItem[K]](f: String => String): DictEx[K, J] =
     DictEx(items.map {item => ADictItem(item.key, f(item.value))})
   def +:[J >: I <: DictItem[K]](item: J): DictEx[K, J] = DictEx(item :: items)
 }
