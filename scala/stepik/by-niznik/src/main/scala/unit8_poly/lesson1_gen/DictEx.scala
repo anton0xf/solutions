c80a57c1abdaa82f15f1b543305b15a72b17b2ae
anton0xf <anton0xf@gmail.com>
2024-05-16 14:00:34 +0300

    stepik/scala/niznik: 8.1. Dict example - set parametrize K

--- scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson1_gen/DictEx.scala
+++ scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson1_gen/DictEx.scala
@@ -1,24 +1,24 @@
 package unit8_poly.lesson1_gen
 
-trait DictItem {
-  def key: Int
+trait DictItem[K] {
+  def key: K
   def value: String
 }
 
-case class DictEx[I <: DictItem](items: List[I]) {
+case class DictEx[K, I <: DictItem[K]](items: List[I]) {
   def head: I = items.head
-  def map(f: String => String): DictEx[ADictItem] =
+  def map(f: String => String): DictEx[K, ADictItem[K]] =
     DictEx(items.map {item => ADictItem(item.key, f(item.value))})
-  def +:[J >: I <: DictItem](item: J): DictEx[J] = DictEx(item :: items)
+  def +:[J >: I <: DictItem[K]](item: J): DictEx[K, J] = DictEx(item :: items)
 }
 
 object DictEx {
-  def apply[I <: DictItem](items: I*): DictEx[I] =
+  def apply[K, I <: DictItem[K]](items: I*): DictEx[K, I] =
     DictEx(items.toList)
 }
 
-case class ADictItem(key: Int, value: String) extends DictItem
-case class TDictItem(p: (Int, String)) extends DictItem {
-  override def key: Int = p._1
+case class ADictItem[K](key: K, value: String) extends DictItem[K]
+case class TDictItem[K](p: (K, String)) extends DictItem[K] {
+  override def key: K = p._1
   override def value: String = p._2
 }
--- scala/stepik/by-niznik/src/test/scala/unit8_poly/lesson1_gen/DictExSpec.scala
+++ scala/stepik/by-niznik/src/test/scala/unit8_poly/lesson1_gen/DictExSpec.scala
@@ -4,21 +4,27 @@ import munit.FunSuite
 
 class DictExSpec extends FunSuite {
   test("head A") {
-    val dict = DictEx(ADictItem(1, "asdf"), ADictItem(2, "qq"))
+    val dict: DictEx[Int, ADictItem[Int]] = DictEx(ADictItem(1, "asdf"), ADictItem(2, "qq"))
     assertEquals(dict.head, ADictItem(1, "asdf"))
   }
   test("head T") {
-    val dict = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val dict: DictEx[Int, TDictItem[Int]] = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
     assertEquals(dict.head, TDictItem(1, "asdf"))
   }
+  test("head AT") {
+    val dict: DictEx[Int, DictItem[Int]] = DictEx(ADictItem(1, "asdf"), TDictItem(2, "qq"))
+    assertEquals(dict.head, ADictItem(1, "asdf"))
+  }
   test("map") {
-    val dict = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
-    assertEquals(dict.map(_ + "_"), DictEx(ADictItem(1, "asdf_"), ADictItem(2, "qq_")))
+    val dict: DictEx[Int, TDictItem[Int]] = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val expected: DictEx[Int, ADictItem[Int]] = DictEx(ADictItem(1, "asdf_"), ADictItem(2, "qq_"))
+    assertEquals(dict.map(_ + "_"), expected)
   }
   test("prepend") {
-    val dict: DictEx[TDictItem] = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
-    val actual: DictEx[DictItem] = ADictItem(3, "aa") +: dict
-    val expected: DictEx[DictItem] = DictEx(ADictItem(3, "aa"), TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val dict: DictEx[Int, TDictItem[Int]] = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val actual: DictEx[Int, DictItem[Int]] = ADictItem(3, "aa") +: dict
+    val expected: DictEx[Int, DictItem[Int]] =
+      DictEx(ADictItem(3, "aa"), TDictItem(1, "asdf"), TDictItem(2, "qq"))
     assertEquals(actual, expected)
   }
 }
