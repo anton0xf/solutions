6d17a7f6a377d4159515a48ff262b0962660c2a7
anton0xf <anton0xf@gmail.com>
2024-05-16 13:20:58 +0300

    stepik/scala/niznik: 8.1. Dict example - set K=Int, V=String

--- scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson1_gen/DictEx.scala
+++ scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson1_gen/DictEx.scala
@@ -1,24 +1,23 @@
 package unit8_poly.lesson1_gen
-// https://stepik.org/lesson/106542/step/2?unit=81068
 
-trait DictItem[K, V] {
-  def key: K
-  def value: V
+trait DictItem {
+  def key: Int
+  def value: String
 }
 
-case class DictEx[K, V, I <: DictItem[K, V]](items: List[I]) {
+case class DictEx[I <: DictItem](items: List[I]) {
   def head: I = items.head
-  def map[U](f: V => U): DictEx[K, U, ADictItem[K, U]] =
+  def map(f: String => String): DictEx[ADictItem] =
     DictEx(items.map {item => ADictItem(item.key, f(item.value))})
 }
 
 object DictEx {
-  def apply[K, V, I <: DictItem[K, V]](items: I*): DictEx[K, V, I] =
+  def apply[I <: DictItem](items: I*): DictEx[I] =
     DictEx(items.toList)
 }
 
-case class ADictItem[K, V](key: K, value: V) extends DictItem[K, V]
-case class TDictItem[K, V](p: (K, V)) extends DictItem[K, V] {
-  override def key: K = p._1
-  override def value: V = p._2
+case class ADictItem(key: Int, value: String) extends DictItem
+case class TDictItem(p: (Int, String)) extends DictItem {
+  override def key: Int = p._1
+  override def value: String = p._2
 }
