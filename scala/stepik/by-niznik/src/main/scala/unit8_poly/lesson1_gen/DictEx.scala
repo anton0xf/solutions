d98c0d03c980950701a78032ccb9a43a80655952
anton0xf <anton0xf@gmail.com>
2024-05-16 18:10:55 +0300

    stepik/scala/niznik: 8.1. Dict example - set parametrize value

--- scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson1_gen/DictEx.scala
+++ scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson1_gen/DictEx.scala
@@ -1,24 +1,24 @@
 package unit8_poly.lesson1_gen
 
-trait DictItem[K] {
+trait DictItem[K, V] {
   def key: K
-  def value: String
+  def value: V
 }
 
-case class DictEx[K, I <: DictItem[K]](items: List[I]) {
+case class DictEx[K, V, I <: DictItem[K, V]](items: List[I]) {
   def head: I = items.head
-  def map[J >: ADictItem[K] <: DictItem[K]](f: String => String): DictEx[K, J] =
+  def map[U, J >: ADictItem[K, U] <: DictItem[K, U]](f: V => U): DictEx[K, U, J] =
     DictEx(items.map {item => ADictItem(item.key, f(item.value))})
-  def +:[J >: I <: DictItem[K]](item: J): DictEx[K, J] = DictEx(item :: items)
+  def +:[J >: I <: DictItem[K, V]](item: J): DictEx[K, V, J] = DictEx(item :: items) // TODO V => U >: V
 }
 
 object DictEx {
-  def apply[K, I <: DictItem[K]](items: I*): DictEx[K, I] =
+  def apply[K, V, I <: DictItem[K, V]](items: I*): DictEx[K, V, I] =
     DictEx(items.toList)
 }
 
-case class ADictItem[K](key: K, value: String) extends DictItem[K]
-case class TDictItem[K](p: (K, String)) extends DictItem[K] {
+case class ADictItem[K, V](key: K, value: V) extends DictItem[K, V]
+case class TDictItem[K, V](p: (K, V)) extends DictItem[K, V] {
   override def key: K = p._1
-  override def value: String = p._2
+  override def value: V = p._2
 }
--- scala/stepik/by-niznik/src/test/scala/unit8_poly/lesson1_gen/DictExSpec.scala
+++ scala/stepik/by-niznik/src/test/scala/unit8_poly/lesson1_gen/DictExSpec.scala
@@ -4,26 +4,45 @@ import munit.FunSuite
 
 class DictExSpec extends FunSuite {
   test("head A") {
-    val dict: DictEx[Int, ADictItem[Int]] = DictEx(ADictItem(1, "asdf"), ADictItem(2, "qq"))
+    val dict: DictEx[Int, String, ADictItem[Int, String]] =
+      DictEx(ADictItem(1, "asdf"), ADictItem(2, "qq"))
     assertEquals(dict.head, ADictItem(1, "asdf"))
   }
+
   test("head T") {
-    val dict: DictEx[Int, TDictItem[Int]] = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val dict: DictEx[Int, String, TDictItem[Int, String]] =
+      DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
     assertEquals(dict.head, TDictItem(1, "asdf"))
   }
+
   test("head AT") {
-    val dict: DictEx[Int, DictItem[Int]] = DictEx(ADictItem(1, "asdf"), TDictItem(2, "qq"))
+    val dict: DictEx[Int, String, DictItem[Int, String]] =
+      DictEx(ADictItem(1, "asdf"), TDictItem(2, "qq"))
     assertEquals(dict.head, ADictItem(1, "asdf"))
   }
+
   test("map") {
-    val dict: DictEx[Int, TDictItem[Int]] = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
-    val expected: DictEx[Int, ADictItem[Int]] = DictEx(ADictItem(1, "asdf_"), ADictItem(2, "qq_"))
+    val dict: DictEx[Int, String, TDictItem[Int, String]] =
+      DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val expected: DictEx[Int, String, ADictItem[Int, String]] =
+      DictEx(ADictItem(1, "asdf_"), ADictItem(2, "qq_"))
     assertEquals(dict.map(_ + "_"), expected)
   }
+
+  test("map to other type") {
+    val dict: DictEx[Int, String, TDictItem[Int, String]] =
+      DictEx(TDictItem(1, "11"), TDictItem(2, "12"))
+    val expected: DictEx[Int, Int, ADictItem[Int, Int]] =
+      DictEx(ADictItem(1, 11), ADictItem(2, 12))
+    assertEquals(dict.map(_.toInt), expected)
+  }
+
   test("prepend") {
-    val dict: DictEx[Int, TDictItem[Int]] = DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
-    val actual: DictEx[Int, DictItem[Int]] = ADictItem(3, "aa") +: dict
-    val expected: DictEx[Int, DictItem[Int]] =
+    val dict: DictEx[Int, String, TDictItem[Int, String]] =
+      DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val actual: DictEx[Int, String, DictItem[Int, String]] =
+      ADictItem(3, "aa") +: dict
+    val expected: DictEx[Int, String, DictItem[Int, String]] =
       DictEx(ADictItem(3, "aa"), TDictItem(1, "asdf"), TDictItem(2, "qq"))
     assertEquals(actual, expected)
   }
