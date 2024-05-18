cc8cd7832d0b94d8be258513fa4c8ac8732b31a2
anton0xf <anton0xf@gmail.com>
2024-05-18 18:11:49 +0300

    stepik/scala/niznik: 8.1. Dict example - set allow to extend prepend result

--- scala/stepik/by-niznik/src/test/scala/unit8_poly/lesson1_gen/DictExSpec.scala
+++ scala/stepik/by-niznik/src/test/scala/unit8_poly/lesson1_gen/DictExSpec.scala
@@ -4,46 +4,56 @@ import munit.FunSuite
 
 class DictExSpec extends FunSuite {
   test("head A") {
-    val dict: DictEx[Int, String, ADictItem[Int, String]] =
-      DictEx(ADictItem(1, "asdf"), ADictItem(2, "qq"))
+    val dict: DictEx[Int, String, ADictItem] = DictEx.make(ADictItem(1, "asdf"), ADictItem(2, "qq"))
     assertEquals(dict.head, ADictItem(1, "asdf"))
   }
 
   test("head T") {
-    val dict: DictEx[Int, String, TDictItem[Int, String]] =
-      DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val dict: DictEx[Int, String, TDictItem] = DictEx.make(TDictItem(1, "asdf"), TDictItem(2, "qq"))
     assertEquals(dict.head, TDictItem(1, "asdf"))
   }
 
   test("head AT") {
-    val dict: DictEx[Int, String, DictItem[Int, String]] =
-      DictEx(ADictItem(1, "asdf"), TDictItem(2, "qq"))
+    val dict: DictEx[Int, String, DictItem] = DictEx.make(ADictItem(1, "asdf"), TDictItem(2, "qq"))
     assertEquals(dict.head, ADictItem(1, "asdf"))
   }
 
   test("map") {
-    val dict: DictEx[Int, String, TDictItem[Int, String]] =
-      DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
-    val expected: DictEx[Int, String, ADictItem[Int, String]] =
-      DictEx(ADictItem(1, "asdf_"), ADictItem(2, "qq_"))
+    val dict: DictEx[Int, String, TDictItem] = DictEx.make(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val expected: DictEx[Int, String, ADictItem] = DictEx.make(ADictItem(1, "asdf_"), ADictItem(2, "qq_"))
     assertEquals(dict.map(_ + "_"), expected)
   }
 
   test("map to other type") {
-    val dict: DictEx[Int, String, TDictItem[Int, String]] =
-      DictEx(TDictItem(1, "11"), TDictItem(2, "12"))
-    val expected: DictEx[Int, Int, ADictItem[Int, Int]] =
-      DictEx(ADictItem(1, 11), ADictItem(2, 12))
+    val dict: DictEx[Int, String, TDictItem] = DictEx.make(TDictItem(1, "11"), TDictItem(2, "12"))
+    val expected: DictEx[Int, Int, ADictItem] = DictEx.make(ADictItem(1, 11), ADictItem(2, 12))
     assertEquals(dict.map(_.toInt), expected)
   }
 
   test("prepend") {
-    val dict: DictEx[Int, String, TDictItem[Int, String]] =
-      DictEx(TDictItem(1, "asdf"), TDictItem(2, "qq"))
-    val actual: DictEx[Int, String, DictItem[Int, String]] =
-      ADictItem(3, "aa") +: dict
-    val expected: DictEx[Int, String, DictItem[Int, String]] =
-      DictEx(ADictItem(3, "aa"), TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val dict: DictEx[Int, String, TDictItem] = DictEx.make(TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    val actual: DictEx[Int, String, DictItem] = ADictItem(3, "aa") +: dict
+    val expected: DictEx[Int, String, DictItem] =
+      DictEx.make(ADictItem(3, "aa"), TDictItem(1, "asdf"), TDictItem(2, "qq"))
+    assertEquals(actual, expected)
+  }
+
+  test("prepend other type") {
+    class A(val n: Int) {
+      override def equals(obj: Any): Boolean =
+        obj.isInstanceOf[A] && obj.asInstanceOf[A].n == n
+    }
+    object A { def apply(n: Int) = new A(n) }
+    class B(n: Int, val s: String) extends A(n) {
+      override def equals(obj: Any): Boolean =
+        super.equals(obj) && obj.isInstanceOf[B] && obj.asInstanceOf[B].s == s
+    }
+    object B { def apply(n: Int, s: String) = new B(n, s) }
+
+    val dict: DictEx[Int, B, DictItem] = DictEx.make(ADictItem(1, B(10, "a")), TDictItem(2, B(20, "qq")))
+    val actual: DictEx[Int, A, DictItem] = ADictItem(3, A(30)) +: dict
+    val expected: DictEx[Int, A, DictItem] =
+      DictEx.make(ADictItem(3, A(30)), ADictItem(1, B(10, "a")), TDictItem(2, B(20, "qq")))
     assertEquals(actual, expected)
   }
 }
