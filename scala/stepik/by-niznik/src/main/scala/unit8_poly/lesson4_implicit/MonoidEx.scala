ec8dae70190e88aae3e8084976c0d4712292f582
anton0xf <anton0xf@gmail.com>
2024-05-21 13:24:54 +0300

    stepik/scala/niznik: 8.4.2. fold using Monoid example with implicits

--- scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson4_implicit/MonoidEx.scala
+++ scala/stepik/by-niznik/src/main/scala/unit8_poly/lesson4_implicit/MonoidEx.scala
@@ -1,32 +1,31 @@
 package unit8_poly.lesson4_implicit
 // https://stepik.org/lesson/106535/step/2?unit=81061
 
-object MonoidEx {
-  private def reduceMonoid[A](xs: List[A])(monoid: Monoid[A]) = xs.foldLeft(monoid.zero)(monoid.+)
-  def concatAll(xs: List[String]): String = reduceMonoid(xs)(StringMonoid)
-  def sumAll(xs: List[Int]): Int = reduceMonoid(xs)(IntMonoid)
-  def forAll(xs: List[Boolean]): Boolean = reduceMonoid(xs)(BoolMonoid)
-}
-
 trait Monoid[A] {
   def zero: A
   def +(x: A, y: A): A
 }
 
-object StringMonoid extends Monoid[String] {
-  override def zero: String = ""
-  override def +(x: String, y: String): String = x + y
+object Monoid {
+  implicit val stringMonoid: Monoid[String] = new Monoid[String] {
+    override def zero: String = ""
+    override def +(x: String, y: String): String = x + y
+  }
+
+  implicit object IntMonoid extends Monoid[Int] {
+    override def zero: Int = 0
+    override def +(x: Int, y: Int): Int = x + y
+  }
+
+  implicit object BoolMonoid extends Monoid[Boolean] {
+    override def zero: Boolean = true
+    override def +(x: Boolean, y: Boolean): Boolean = x && y
+  }
 }
 
-object IntMonoid extends Monoid[Int] {
-  override def zero: Int = 0
-  override def +(x: Int, y: Int): Int = x + y
+object MonoidEx {
+  private def reduceMonoid[A](xs: List[A])(implicit monoid: Monoid[A]): A = xs.foldLeft(monoid.zero)(monoid.+)
+  def concatAll(xs: List[String]): String = reduceMonoid(xs)
+  def sumAll(xs: List[Int]): Int = reduceMonoid(xs)
+  def forAll(xs: List[Boolean]): Boolean = reduceMonoid(xs)
 }
-
-object BoolMonoid extends Monoid[Boolean] {
-  override def zero: Boolean = true
-  override def +(x: Boolean, y: Boolean): Boolean = x && y
-}
-
-
-
