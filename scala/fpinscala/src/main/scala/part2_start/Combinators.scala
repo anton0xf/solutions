1ab5d337ff06ab9b6392847ba7434eb390050c35
anton0xf <anton0xf@gmail.com>
2024-06-20 01:13:00 +0300

    FP in Scala: ex 2.5. compose

--- scala/fpinscala/src/main/scala/part2_start/Combinators.scala
+++ scala/fpinscala/src/main/scala/part2_start/Combinators.scala
@@ -1,6 +1,7 @@
 package part2_start
 
 object Combinators {
-  def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+  def curry[A, B, C](f: (A, B) => C): A => B => C = a => b => f(a, b)
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+  def compose[A, B, C](f: B => C, g: A => B): A => C = x => f(g(x))
 }
