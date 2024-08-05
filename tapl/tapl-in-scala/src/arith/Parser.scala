a64d7617e3b75877cec62c4dfa23bd228c8d0309
anton0xf <anton0xf@gmail.com>
2024-08-05 23:34:36 +0300

    tapl/scala: state monad

--- tapl/tapl-in-scala/src/arith/Parser.scala
+++ tapl/tapl-in-scala/src/arith/Parser.scala
@@ -9,45 +9,53 @@ enum Term:
   case Pred(x: Term)
   case IsZero(x: Term)
 
-type ParseState = (Term, List[Token])
+case class ParseState[A](runSt: List[Token] => (A, List[Token])) {
+  def flatMap[B](f: A => ParseState[B]): ParseState[B] = ParseState { (tokens: List[Token]) =>
+    val (x, rest1) = runSt(tokens)
+    f(x).runSt(rest1)
+  }
 
-object ParseState {
-  def apply(term: Term, rest: List[Token]): ParseState = (term, rest)
+  def map[B](f: A => B): ParseState[B] = flatMap((x: A) => ParseState { (f(x), _) })
+
+  def <*[B](st: ParseState[B]): ParseState[A] = for {
+    x <- this
+    _ <- st
+  } yield x
 }
 
 object Parser {
   def parse(tokens: List[Token]): Term =
-    val (term, rest) = parseExpr(tokens)
+    val (term, rest) = parseExpr.runSt(tokens)
     if rest.nonEmpty
     then throw new RuntimeException(s"Unexpected token ${rest.head}. Expected end of input")
     else term
 
   def toTokens(ast: Term): List[Token] = ???
 
-  def thenIgnore(state: ParseState, token: Token): ParseState = state match
-    case (_, Nil) => throw new RuntimeException(s"Unexpected end of input. Expected $token")
-    case (x, token :: rest) => (x, rest)
+  def skip(token: Token): ParseState[Unit] = ParseState {
+    case Nil           => throw new RuntimeException(s"Unexpected end of input. Expected $token")
+    case token :: rest => ((), rest)
+  }
 
-  def thenMap(state: ParseState, f: Term => Term): ParseState = (f(state._1), state._2)
-
-  def parseExpr(tokens: List[Token]): ParseState = tokens match
+  def parseExpr: ParseState[Term] = ParseState {
     case Nil                 => throw new RuntimeException("Unexpected end of input")
-    case True :: rest        => ParseState(Term.True, rest)
-    case False :: rest       => ParseState(Term.False, rest)
-    case Zero :: rest        => ParseState(Term.Zero, rest)
-    case OpenBracket :: rest => thenIgnore(parseExpr(rest), CloseBracket)
+    case True :: rest        => (Term.True, rest)
+    case False :: rest       => (Term.False, rest)
+    case Zero :: rest        => (Term.Zero, rest)
+    case OpenBracket :: rest => (parseExpr <* skip(CloseBracket)).runSt(rest)
     case CloseBracket :: _   => throw new RuntimeException("Unexpected close bracket")
-    case Succ :: rest        => thenMap(parseExpr(rest), Term.Succ(_))
-    case Pred :: rest        => thenMap(parseExpr(rest), Term.Pred(_))
-    case IsZero :: rest      => thenMap(parseExpr(rest), Term.IsZero(_))
-    case If :: rest          => parseIf(rest)
+    case Succ :: rest        => parseExpr.map(Term.Succ(_)).runSt(rest)
+    case Pred :: rest        => parseExpr.map(Term.Pred(_)).runSt(rest)
+    case IsZero :: rest      => parseExpr.map(Term.IsZero(_)).runSt(rest)
+    case If :: rest          => parseIf.runSt(rest)
     case Else :: _           => throw new RuntimeException("Unexpected 'else'")
     case Then :: _           => throw new RuntimeException("Unexpected 'then'")
-
-  def parseIf(tokens: List[Token]): ParseState = {
-    val (condT, rest1) = thenIgnore(parseExpr(tokens), Then)
-    val (thenT, rest2) = thenIgnore(parseExpr(rest1), Else)
-    val (elseT, rest3) = parseExpr(rest2)
-    (Term.If(condT, thenT, elseT), rest3)
   }
+
+  def parseIf: ParseState[Term] =
+    for {
+      condT <- parseExpr <* skip(Then)
+      thenT <- parseExpr <* skip(Else)
+      elseT <- parseExpr
+    } yield Term.If(condT, thenT, elseT)
 }
