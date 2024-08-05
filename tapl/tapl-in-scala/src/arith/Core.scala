9c6a49a7e549fb72e9ced703c0232a677923e82a
anton0xf <anton0xf@gmail.com>
2024-08-05 22:16:07 +0300

    tapl/scala: extract evalProgram

--- tapl/tapl-in-scala/src/arith/Core.scala
+++ tapl/tapl-in-scala/src/arith/Core.scala
@@ -2,4 +2,12 @@ package arith
 
 object Core {
   def eval(ast: Term): Term = ???
+
+  def evalProgram(program: String): String = {
+    val tokens = Lexer.tokenize(program)
+    val ast: Term = Parser.parse(tokens)
+    val res: Term = Core.eval(ast)
+    val resTokens: List[Token] = Parser.toTokens(ast)
+    resTokens.map(Lexer.tokenToStr).mkString(" ")
+  }
 }
--- tapl/tapl-in-scala/src/arith/Main.scala
+++ tapl/tapl-in-scala/src/arith/Main.scala
@@ -7,9 +7,6 @@ def eval(args: String*): Unit = {
   val program = (
     if args.nonEmpty then Source.fromFile(args(0)) else Source.stdin
   ).getLines().mkString
-  val tokens = Lexer.tokenize(program)
-  val ast: Term = Parser.parse(tokens)
-  val res: Term = Core.eval(ast)
-  val resTokens: List[Token] = Parser.toTokens(ast)
-  println(resTokens.map(Lexer.tokenToStr).mkString(" "))
+  val out: String = Core.evalProgram(program)
+  println(out)
 }
