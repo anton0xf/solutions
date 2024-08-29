1ad27986a89792f4e6f8d3ba314efa4c37dd403e
anton0xf <anton0xf@gmail.com>
2024-08-29 18:25:53 +0300

    coursera/sfp/w1: isGoodEnough adhoc solution

--- scala/coursera/sfp/w1_example/src/main/scala/example/SqrtIter.scala
+++ scala/coursera/sfp/w1_example/src/main/scala/example/SqrtIter.scala
@@ -1,11 +1,11 @@
 package example
 
 import scala.annotation.tailrec
-import scala.math.abs
+import scala.math.{abs, max}
 
 object SqrtIter {
   def improve(guess: Double, x: Double): Double = (guess + x / guess) / 2
-  def isGoodEnough(guess: Double, x: Double): Boolean = abs(guess*guess - x) < 0.001
+  def isGoodEnough(guess: Double, x: Double): Boolean = abs(guess - x/guess) < max(1, abs(guess)) / 1000
   def sqrt(x: Double): Double =
     @tailrec
     def go(guess: Double): Double = if isGoodEnough(guess, x) then guess else go(improve(guess, x))
--- scala/coursera/sfp/w1_example/src/main/scala/example/sqrt_test.sc
+++ scala/coursera/sfp/w1_example/src/main/scala/example/sqrt_test.sc
@@ -5,5 +5,22 @@ isGoodEnough(0.01, 0.0001)
 isGoodEnough(0.001, 0.000001)
 isGoodEnough(1e-20, 1e-40)
 isGoodEnough(1e-21, 1e-42)
+1e-42 / 1e-21
+1e-21 - 1e-42 / 1e-21
+
+isGoodEnough(1e-50, 1e-100)
 isGoodEnough(1e20, 1e40+0.1)
 isGoodEnough(1e50, 1e100) // false
+1e50 - 1e100 / 1e50
+
+sqrt(1e-100)
+sqrt(1e-40)
+sqrt(1e-20)
+sqrt(0.0001)
+sqrt(0.01)
+sqrt(4)
+sqrt(100)
+sqrt(10000)
+sqrt(1e20)
+sqrt(1e40)
+sqrt(1e100)
