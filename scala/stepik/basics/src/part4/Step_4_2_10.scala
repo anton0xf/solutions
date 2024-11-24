1bd8d3e4a32a6f5f7ba49bd86003d61892a05c6a
anton0xf <anton0xf@gmail.com>
2024-11-24 22:32:43 +0500

    scala/stepik/basics: 4.2.10: fix build (remove some variants)

--- scala/stepik/basics/src/part4/Step_4_2_10.scala
+++ scala/stepik/basics/src/part4/Step_4_2_10.scala
@@ -7,27 +7,19 @@ object Step_4_2_10 {
     val phone = "9-876-543-21-09"
     printMap(countNumbers(phone))
     printMap(countNumbers0(phone))
-    printMap(countNumbers1(phone))
-    printMap(countNumbers2(phone))
   }
 
-  private def printMap(m: Map[Char, Int]) = {
+  private def printMap(m: Map[Char, Int]): Unit = {
     val kvs: List[Int] = for { k <- List.range(0, 10) } yield m(('0' + k).toChar)
     println(kvs.mkString(""))
   }
 
   private def countNumbers(s: String): Map[Char, Int] =
-    (s.groupBy(identity) - '-').view.mapValues(_.length).toMap
+    (s.groupBy(identity) - '-').mapValues(_.length)
 
   private def countNumbers0(s: String): Map[Char, Int] =
     s.foldLeft(Map[Char, Int]().withDefaultValue(0)) { case (acc, letter) =>
       if (letter != '-') acc + (letter -> (1 + acc(letter)))
       else acc
     }
-
-  private def countNumbers1(s: String): Map[Char, Int] =
-    s.groupMapReduce(identity)(_ => 1)(_ + _).removed('-')
-
-  private def countNumbers2(s: String): Map[Char, Int] =
-    new WrappedString(s).groupBy(identity).removed('-').view.mapValues(_.length).toMap
 }
