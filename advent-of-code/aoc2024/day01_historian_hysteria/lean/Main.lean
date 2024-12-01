4632264573b52706a2db6765a1a92299cc1755ce
anton0xf <anton0xf@gmail.com>
2024-12-01 19:38:59 +0300

    aoc2024/day01_historian_hysteria(lean): done

--- advent-of-code/aoc2024/day01_historian_hysteria/lean/Main.lean
+++ advent-of-code/aoc2024/day01_historian_hysteria/lean/Main.lean
@@ -1,4 +1,19 @@
-import Day01
+def main : IO Unit := do
+  let input <- IO.FS.readFile "../input"
+  let lines := input.splitOn "\n"
+  let pairs := (lines.filter (fun line => not line.isEmpty)).map (
+    fun line => match (line.splitOn "   ").map (String.toInt!) with
+    | x :: [y] => (x, y)
+    | lst => panic s!"not a pair {lst} from {line}")
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  let (xs, ys) := pairs.unzip
+  let xss := xs.mergeSort
+  let yss := ys.mergeSort
+  let ps: List (Int × Int) := xss.zip yss
+  let ds := ps.map (fun (x, y) => (x - y).natAbs)
+  let res1 := ds.foldl Nat.add 0
+  IO.println s!"part 1: {res1}"
+
+  let dd := xss.map (fun x => x * yss.count x)
+  let res2 := dd.foldl (· + ·) 0
+  IO.println s!"part 2: {res2}"
