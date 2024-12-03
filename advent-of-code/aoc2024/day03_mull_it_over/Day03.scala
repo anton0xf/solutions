3f3c5d1074621aafb24e70b28040fdb62dab7c3d
anton0xf <anton0xf@gmail.com>
2024-12-03 16:19:19 +0300

    aoc2024/day03_mull_it_over: finish part 2

--- advent-of-code/aoc2024/day03_mull_it_over/Day03.scala
+++ advent-of-code/aoc2024/day03_mull_it_over/Day03.scala
@@ -3,16 +3,59 @@ import scala.io.Source
 import scala.util.matching.Regex
 
 object Day03 {
-  val re: Regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)".r
+  val re1: Regex = """mul\((\d{1,3}),(\d{1,3})\)""".r
+  val re2: Regex = """(do)\(\)|(don't)\(\)|(mul)\((\d{1,3}),(\d{1,3})\)""".r
 
   def main(args: Array[String]): Unit = {
     Using(Source.fromFile("day03_mull_it_over/input")) { source =>
       val input = source.getLines().mkString("\n")
-      // val reports = lines.map(_.split("\\s+").map(_.toInt).toList).toList
-      val muls = for { m <- re.findAllMatchIn(input) }
-        yield (BigInt(m.group(1)), BigInt(m.group(2)))
-      val res1 = muls.map((x,y) => x * y).sum
+      // part 1
+      val res1 = solution1(input)
       println(s"part 1: $res1")
+
+      // part 2
+      val res2 = solution2(input)
+      println(s"part 2: $res2")
+    }
+  }
+
+  def solution1(input: String): BigInt = {
+    val muls = for {
+      m <- re1.findAllMatchIn(input)
+    } yield (BigInt(m.group(1)), BigInt(m.group(2)))
+    muls.map((x, y) => x * y).sum
+  }
+
+  private enum Command {
+    case Do(flag: Boolean)
+    case Mul(x: BigInt, y: BigInt)
+  }
+  import Day03.Command.*
+
+  def solution2(input: String): BigInt = {
+    val commands = for {
+      m <- re2.findAllMatchIn(input)
+    } yield getCommand2(m)
+    commands.foldLeft(EvalState(true, 0))(eval).sum
+  }
+
+  private def getCommand2(m: Regex.Match): Command = {
+    (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5)) match {
+      case ("do", null, null, null, null)    => Do(true)
+      case (null, "don't", null, null, null) => Do(false)
+      case (null, null, "mul", s1, s2)       => Mul(BigInt(s1), BigInt(s2))
+    }
+  }
+
+  private case class EvalState(mulEnabled: Boolean, sum: BigInt)
+
+  private def eval(state: EvalState, command: Command) = {
+    command match {
+      case Do(flag) => state.copy(mulEnabled = flag)
+      case Mul(x, y) =>
+        if state.mulEnabled
+        then state.copy(sum = state.sum + (x * y))
+        else state
     }
   }
 }
--- advent-of-code/aoc2024/day03_mull_it_over/Day03Test.scala
+++ advent-of-code/aoc2024/day03_mull_it_over/Day03Test.scala
@@ -2,34 +2,34 @@
 import Day03.*
 
 class Day03Test extends munit.FunSuite {
-  test("re.matches") {
-    assertEquals(re.matches("mul(1,2)"), true)
-    assertEquals(re.matches("mul( 1,2)"), false)
-    assertEquals(re.matches("mul(,2)"), false)
-    assertEquals(re.matches("mul(1,22)"), true)
-    assertEquals(re.matches("mul(123,222)"), true)
-    assertEquals(re.matches("mul(1,1234)"), false)
-    assertEquals(re.matches("mul(1,123 )"), false)
-    assertEquals(re.matches("mul(1,123 "), false)
+  test("re1.matches") {
+    assertEquals(re1.matches("mul(1,2)"), true)
+    assertEquals(re1.matches("mul( 1,2)"), false)
+    assertEquals(re1.matches("mul(,2)"), false)
+    assertEquals(re1.matches("mul(1,22)"), true)
+    assertEquals(re1.matches("mul(123,222)"), true)
+    assertEquals(re1.matches("mul(1,1234)"), false)
+    assertEquals(re1.matches("mul(1,123 )"), false)
+    assertEquals(re1.matches("mul(1,123 "), false)
   }
-  test("re.findPrefixMatchOf") {
-    val mOpt = re.findPrefixMatchOf("mul(1,23)")
+  test("re1.findPrefixMatchOf") {
+    val mOpt = re1.findPrefixMatchOf("mul(1,23)")
     assert(mOpt.isDefined)
     val m = mOpt.get
     assertEquals(m.groupCount, 2)
     assertEquals(m.group(1), "1")
     assertEquals(m.group(2), "23")
   }
-  test("re.findFirstIn") {
-    val mOpt = re.findFirstMatchIn("asdf*mul mul(1,23)")
+  test("re1.findFirstIn") {
+    val mOpt = re1.findFirstMatchIn("asdf*mul mul(1,23)")
     assert(mOpt.isDefined)
     val m = mOpt.get
     assertEquals(m.groupCount, 2)
     assertEquals(m.group(1), "1")
     assertEquals(m.group(2), "23")
   }
-  test("re.findAllMatchIn") {
-    val ms = re.findAllMatchIn("asdf*mul mul(1,23)mul(12,3456)mul(333,21)")
+  test("re1.findAllMatchIn") {
+    val ms = re1.findAllMatchIn("asdf*mul mul(1,23)mul(12,3456)mul(333,21)")
 
     assert(ms.hasNext)
     val m1 = ms.next()
@@ -44,5 +44,61 @@ class Day03Test extends munit.FunSuite {
     assertEquals(m2.group(2), "21")
 
     assert(!ms.hasNext)
+  }
+  test("solution1") {
+    val input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    assertEquals(solution1(input), BigInt(161))
+  }
+  // part 2
+  test("re2.matches") {
+    assertEquals(re2.matches("mul(1,2)"), true)
+    assertEquals(re2.matches("mul( 1,2)"), false)
+    assertEquals(re2.matches("mul(,2)"), false)
+    assertEquals(re2.matches("mul(1,22)"), true)
+    assertEquals(re2.matches("mul(123,222)"), true)
+    assertEquals(re2.matches("mul(1,1234)"), false)
+    assertEquals(re2.matches("mul(1,123 )"), false)
+    assertEquals(re2.matches("mul(1,123 "), false)
+    assertEquals(re2.matches("do()"), true)
+    assertEquals(re2.matches("do"), false)
+    assertEquals(re2.matches("do(1,2)"), false)
+    assertEquals(re2.matches("don't()"), true)
+  }
+  test("re2.findPrefixMatchOf mul") {
+    val mOpt = re2.findPrefixMatchOf("mul(1,23)")
+    assert(mOpt.isDefined)
+    val m = mOpt.get
+    assertEquals(m.groupCount, 5)
+    assertEquals(m.group(1), null)
+    assertEquals(m.group(2), null)
+    assertEquals(m.group(3), "mul")
+    assertEquals(m.group(4), "1")
+    assertEquals(m.group(5), "23")
+  }
+  test("re2.findPrefixMatchOf do") {
+    val mOpt = re2.findPrefixMatchOf("do()")
+    assert(mOpt.isDefined)
+    val m = mOpt.get
+    assertEquals(m.groupCount, 5)
+    assertEquals(m.group(1), "do")
+    assertEquals(m.group(2), null)
+    assertEquals(m.group(3), null)
+    assertEquals(m.group(4), null)
+    assertEquals(m.group(5), null)
+  }
+  test("re2.findPrefixMatchOf don't") {
+    val mOpt = re2.findPrefixMatchOf("don't()")
+    assert(mOpt.isDefined)
+    val m = mOpt.get
+    assertEquals(m.groupCount, 5)
+    assertEquals(m.group(1), null)
+    assertEquals(m.group(2), "don't")
+    assertEquals(m.group(3), null)
+    assertEquals(m.group(4), null)
+    assertEquals(m.group(5), null)
+  }
+  test("solution2") {
+    val input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    assertEquals(solution2(input), BigInt(48))
   }
 }
