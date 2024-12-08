46f944d44af68b521865509746309061f9357070
anton0xf <anton0xf@gmail.com>
2024-12-08 13:17:12 +0300

    aoc2024/day07_bridge_repair: finish part 2

--- advent-of-code/aoc2024/day07_bridge_repair/Day07.scala
+++ advent-of-code/aoc2024/day07_bridge_repair/Day07.scala
@@ -5,8 +5,8 @@ object Day07 {
   def main(args: Array[String]): Unit = {
     Using(Source.fromFile("day07_bridge_repair/input")) { source =>
       val input = source.getLines().map(parseEq).toList
-      val res1 = solution1(input)
-      println(s"part 1: $res1")
+      println(s"part 1: ${solution1(input)}")
+      println(s"part 2: ${solution2(input)}")
     }
   }
 
@@ -20,16 +20,35 @@ object Day07 {
 
   // part 1
   def solution1(input: Input): BigInt = input.flatMap(solveEq).sum
+
   def solveEq(eq: Eq): Option[BigInt] = {
-    def go(target: BigInt, args: List[BigInt]): Option[BigInt] = {
-      args match
-        case Nil => None
-        case List(x) => if target == x then Some(eq.res) else None
-        case x :: xs =>
-          go(target - x, xs).orElse {
-            if target % x == 0 then go(target / x, xs) else None
+    def go(target: BigInt, args: List[BigInt]): Option[BigInt] = args match
+      case Nil     => None
+      case List(x) => if target == x then Some(eq.res) else None
+      case x :: xs =>
+        go(target - x, xs)
+          .orElse { if target % x == 0 then go(target / x, xs) else None }
+    go(eq.res, eq.args.reverse)
+  }
+
+  // part 2
+  def solution2(input: Input): BigInt = input.flatMap(solveEq2).sum
+
+  def solveEq2(eq: Eq): Option[BigInt] = {
+    def go(target: BigInt, args: List[BigInt]): Option[BigInt] = args match
+      case Nil     => None
+      case List(x) => if target == x then Some(eq.res) else None
+      case x :: xs =>
+        go(target - x, xs)
+          .orElse { if target % x == 0 then go(target / x, xs) else None }
+          .orElse {
+            val tStr = target.toString()
+            val xStr = x.toString()
+            if tStr == xStr then go(BigInt(0), xs)
+            else if tStr.endsWith(xStr)
+            then go(BigInt(tStr.substring(0, tStr.length - xStr.length)), xs)
+            else None
           }
-    }
-    go(eq.res , eq.args.reverse)
+    go(eq.res, eq.args.reverse)
   }
 }
--- advent-of-code/aoc2024/day07_bridge_repair/Day07Test.scala
+++ advent-of-code/aoc2024/day07_bridge_repair/Day07Test.scala
@@ -27,4 +27,22 @@ class Day07Test extends munit.FunSuite {
   test("solution1") {
     assertEquals(solution1(input), BigInt(3749))
   }
+  // part 2
+  test("solveEq2") {
+    assertEquals(solveEq2(parseEq("156: 15 6")), Some(BigInt(156)))
+    assertEquals(solveEq2(parseEq("45: 1 3 5")), Some(BigInt(45)))
+    assertEquals(solveEq2(parseEq("7290: 6 8 6 15")), Some(BigInt(7290)))
+    assertEquals(solveEq2(parseEq("192: 17 8 14")), Some(BigInt(192)))
+    assertEquals(solveEq2(Eq(BigInt("18021496390543"), List(5, 90, 53, 6, 1, 40, 976, 36, 4))), None)
+    assertEquals(solveEq2(parseEq("15: 15")), Some(BigInt(15)))
+    assertEquals(solveEq2(parseEq("15: 0 15")), Some(BigInt(15)))
+    assertEquals(solveEq2(parseEq("15: 2 15")), None)
+    assertEquals(solveEq2(parseEq("618: 3 100 456 57 5")), None)
+  }
+  test("solveEq2'") {
+    assertEquals(solveEq2(parseEq("618: 3 100 456 57 5")), None)
+  }
+  test("solution2") {
+    assertEquals(solution2(input), BigInt(11387))
+  }
 }
