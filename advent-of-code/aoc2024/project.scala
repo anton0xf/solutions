4882126d7cd85f349757f365b6b012f4521a2ed8
anton0xf <anton0xf@gmail.com>
2024-12-14 13:50:49 +0300

    aoc2024: ignore scratch files in scala-cli project file

--- advent-of-code/aoc2024/project.scala
+++ advent-of-code/aoc2024/project.scala
@@ -4,6 +4,9 @@
 // Main
 //> using dependencies "org.scala-lang.modules::scala-parser-combinators:2.4.0"
 
+// exclude .sc (scratch files)
+//> using exclude "*.sc"
+
 // Test
 // place all tests in 'test' directory
 //> using test.dependencies "org.scalameta::munit::1.0.3"
