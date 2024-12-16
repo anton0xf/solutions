cab16d6e3a1ca77cc9e7faf26a9adf1ab91e9153
anton0xf <anton0xf@gmail.com>
2024-12-16 12:05:21 +0300

    aoc2024/day15_warehouse_woes: part 2

--- advent-of-code/aoc2024/day15_warehouse_woes/Day15.scala
+++ advent-of-code/aoc2024/day15_warehouse_woes/Day15.scala
@@ -1,7 +1,8 @@
 import Day15.Obj.Box
 
-import scala.annotation.targetName
+import scala.annotation.{tailrec, targetName}
 import scala.collection.immutable
+import scala.collection.immutable.Queue
 import scala.util.Using
 import scala.io.Source
 
@@ -10,6 +11,7 @@ object Day15 {
     Using(Source.fromFile("day15_warehouse_woes/input")) { source =>
       val input = parseInput(source.getLines())
       println(s"part 1: ${solution1(input)}")
+      println(s"part 2: ${solution2(input)}")
     }
   }
 
@@ -25,6 +27,9 @@ object Day15 {
   case class Vec(x: Int, y: Int) {
     @targetName("plus")
     def +(p: Vec): Vec = Vec(x + p.x, y + p.y)
+    def incX: Vec = Vec(x + 1, y)
+    def decX: Vec = Vec(x - 1, y)
+    def doubleX: Vec = Vec(x * 2, y)
 
     def gps: Int = x + y * 100
   }
@@ -58,9 +63,10 @@ object Day15 {
   // part 1
   def solution1(input: Input): Int = {
     val end = simulate(input)
-    end.map.iterator.map((p, x) => if x == Box then p.gps else 0).sum
+    end.map.iterator.map((p, x) => if x == Obj.Box then p.gps else 0).sum
   }
 
+  @tailrec
   def simulate(data: Input): Input = data.cmds match
     case Nil => data
     case cmd :: cmds =>
@@ -69,16 +75,15 @@ object Day15 {
 
   def step(cmd: Vec, p: Vec, map: Map[Vec, Obj]): (Vec, Map[Vec, Obj]) = {
     val next = p + cmd
-    val x = map.get(next)
-    x match
-      case None => (next, map)
+    map.get(next) match
+      case None           => (next, map)
       case Some(Obj.Wall) => (p, map)
       case Some(Obj.Box) =>
         val next2 = LazyList.iterate(next)(_ + cmd).find(!map.get(_).contains(Obj.Box)).get
         map.get(next2) match
-          case None => (next, move(map)(next, next2))
+          case None           => (next, move(map)(next, next2))
           case Some(Obj.Wall) => (p, map)
-          case Some(Obj.Box) => throw new Exception(s"unexpected Box at $next2")
+          case Some(Obj.Box)  => throw new Exception(s"unexpected Box at $next2")
   }
 
   def move(map: Map[Vec, Obj])(p0: Vec, p1: Vec): Map[Vec, Obj] =
@@ -86,4 +91,106 @@ object Day15 {
     map.removed(p0).updated(p1, x)
 
   // part 2
+
+  enum Obj2 { case BoxL, BoxR, Wall }
+  case class State2(
+      /** robot position */
+      p: Vec,
+      /** warehouse map */
+      map: Map[Vec, Obj2]
+  ) {
+    def show: String = (0 to map.keys.map(_.y).max)
+      .map { y =>
+        (0 to map.keys.map(_.x).max).map { x =>
+          val pos = Vec(x, y)
+          map.get(pos) match
+            case None            => if pos == p then '@' else '.'
+            case Some(Obj2.Wall) => '#'
+            case Some(Obj2.BoxL) => '['
+            case Some(Obj2.BoxR) => ']'
+        }.mkString
+      }
+      .mkString("\n")
+  }
+
+  def solution2(input: Input): Int = {
+    val state = toState2(input)
+    val end = simulate2(input.cmds, state)
+    end.map.iterator.map((p, x) => if x == Obj2.BoxL then p.gps else 0).sum
+  }
+
+  def toState2(input: Input) = State2(
+    input.p.doubleX,
+    input.map.flatMap((p, x) =>
+      Seq(p.doubleX, p.doubleX.incX) zip (x match
+        case Obj.Wall => Seq(Obj2.Wall, Obj2.Wall)
+        case Obj.Box  => Seq(Obj2.BoxL, Obj2.BoxR)
+      )
+    )
+  )
+
+  @tailrec
+  def simulate2(cmds: List[Vec], state: State2): State2 = cmds match
+    case Nil         => state
+    case cmd :: cmds => simulate2(cmds, step2(cmd, state))
+
+  def step2(cmd: Vec, state: State2): State2 = {
+    val next = state.p + cmd
+    state.map.get(next) match
+      case None            => state.copy(p = next)
+      case Some(Obj2.Wall) => state
+      case Some(_) =>
+        findMovableCluster(cmd, next, state.map) match
+          case None          => state
+          case Some(cluster) => State2(next, move2(cmd, state.map)(cluster))
+  }
+
+  def findMovableCluster(cmd: Vec, start: Vec, map: Map[Vec, Obj2]): Option[List[(Vec, Obj2)]] = {
+    @tailrec
+    def go(q: Queue[Vec], cluster: List[(Vec, Obj2)]): Option[List[(Vec, Obj2)]] = {
+      if q.isEmpty then Some(cluster)
+      else {
+        val (p, q1) = q.dequeue
+        map.get(p) match
+          case None            => go(q1, cluster)
+          case Some(Obj2.Wall) => None
+          case Some(Obj2.BoxL) =>
+            val pair = p.incX
+            assert(map.get(pair).contains(Obj2.BoxR))
+            val next =
+              if cmd.y != 0
+              then Seq(p, pair).map(_ + cmd)
+              else { assert(cmd.x == 1); Seq(pair.incX) }
+            go(q1 :++ next, (p -> Obj2.BoxL) :: (pair -> Obj2.BoxR) :: cluster)
+          case Some(Obj2.BoxR) =>
+            val pair = p.decX
+            assert(map.get(pair).contains(Obj2.BoxL))
+            val next =
+              if cmd.y != 0
+              then Seq(p, pair).map(_ + cmd)
+              else { assert(cmd.x == -1); Seq(pair.decX) }
+            go(q1 :++ next, (pair -> Obj2.BoxL) :: (p -> Obj2.BoxR) :: cluster)
+      }
+    }
+    go(Queue(start), Nil)
+  }
+
+  def move2(cmd: Vec, map: Map[Vec, Obj2])(cluster: List[(Vec, Obj2)]): Map[Vec, Obj2] =
+    map -- cluster.map(_._1) ++ cluster.map((p, x) => (p + cmd, x))
+
+  def parseMap2(lines: IterableOnce[String]): State2 = {
+    val ps = (for {
+      (line, y) <- lines.iterator.zipWithIndex
+      (ch, x) <- line.zipWithIndex
+      if ch != '.'
+    } yield Vec(x, y) -> (ch match
+      case '#' => Left(Obj2.Wall)
+      case '[' => Left(Obj2.BoxL)
+      case ']' => Left(Obj2.BoxR)
+      case '@' => Right(())
+    )).toList
+    val map = ps.flatMap((p, x) => x.swap.toOption.map(p -> _)).toMap
+    val robot = ps.find(_._2.isRight).get._1
+    State2(robot, map)
+  }
 }
--- advent-of-code/aoc2024/day15_warehouse_woes/Day15Test.scala
+++ advent-of-code/aoc2024/day15_warehouse_woes/Day15Test.scala
@@ -50,7 +50,7 @@ class Day15Test extends munit.FunSuite {
   test("parseInput") {
     val data = parseInput(input0)
     assertEquals(data.map.toList.filter((_, x) => x == Obj.Box).map((p, _) => p), List(Vec(2, 1)))
-    assertEquals(data.p, Vec(3,1))
+    assertEquals(data.p, Vec(3, 1))
     assertEquals(data.cmds, List(Vec(-1, 0), Vec(0, -1), Vec(-1, 0), Vec(1, 0)))
   }
 
@@ -62,4 +62,149 @@ class Day15Test extends munit.FunSuite {
   }
 
   // part 2
+  val input3: List[String] =
+    """#######
+      |#...#.#
+      |#.....#
+      |#..OO@#
+      |#..O..#
+      |#.....#
+      |#######
+      |
+      |<vv<<^^<<^^""".stripMargin.lines().toScala(List)
+
+  test("toState2") {
+    assertEquals(
+      toState2(parseInput(input3)).show,
+      """##############
+        |##......##..##
+        |##..........##
+        |##....[][]@.##
+        |##....[]....##
+        |##..........##
+        |##############""".stripMargin
+    )
+  }
+
+  test("simulate2") {
+    val init = toState2(parseInput(input3))
+    assertEquals(
+      simulate2(Nil, init).show,
+      """##############
+        |##......##..##
+        |##..........##
+        |##....[][]@.##
+        |##....[]....##
+        |##..........##
+        |##############""".stripMargin
+    )
+    assertEquals(
+      simulate2("<".map(dirs.apply).toList, init).show,
+      """##############
+        |##......##..##
+        |##..........##
+        |##...[][]@..##
+        |##....[]....##
+        |##..........##
+        |##############""".stripMargin
+    )
+    assertEquals(
+      simulate2("<v".map(dirs.apply).toList, init).show,
+      """##############
+        |##......##..##
+        |##..........##
+        |##...[][]...##
+        |##....[].@..##
+        |##..........##
+        |##############""".stripMargin
+    )
+    assertEquals(
+      simulate2("<vv<".map(dirs.apply).toList, init).show,
+      """##############
+        |##......##..##
+        |##..........##
+        |##...[][]...##
+        |##....[]....##
+        |##......@...##
+        |##############""".stripMargin
+    )
+    assertEquals(
+      simulate2("<vv<<".map(dirs.apply).toList, init).show,
+      """##############
+        |##......##..##
+        |##..........##
+        |##...[][]...##
+        |##....[]....##
+        |##.....@....##
+        |##############""".stripMargin
+    )
+    assertEquals(
+      simulate2("<vv<<^".map(dirs.apply).toList, init).show,
+      """##############
+        |##......##..##
+        |##...[][]...##
+        |##....[]....##
+        |##.....@....##
+        |##..........##
+        |##############""".stripMargin
+    )
+    assertEquals(
+      simulate2("<vv<<^^".map(dirs.apply).toList, init).show,
+      """##############
+        |##......##..##
+        |##...[][]...##
+        |##....[]....##
+        |##.....@....##
+        |##..........##
+        |##############""".stripMargin
+    )
+  }
+
+  test("step2") {
+    val input =
+      """##############
+        |##......##..##
+        |##..........##
+        |##...[][]...##
+        |##....[]....##
+        |##.....@....##
+        |##############""".stripMargin.lines().toScala(List)
+    val state: State2 = parseMap2(input)
+    assertEquals(
+      step2(dirs('^'), state).show,
+      """##############
+        |##......##..##
+        |##...[][]...##
+        |##....[]....##
+        |##.....@....##
+        |##..........##
+        |##############""".stripMargin
+    )
+  }
+
+  test("findMovableCluster") {
+    val input =
+      """##############
+        |##......##..##
+        |##..........##
+        |##...[][]...##
+        |##....[]....##
+        |##.....@....##
+        |##############""".stripMargin.lines().toScala(List)
+    val state: State2 = parseMap2(input)
+    assertEquals(
+      findMovableCluster(Vec(0, -1), Vec(7, 4), state.map),
+      Some(List(
+        Vec(5, 3) -> Obj2.BoxL, Vec(6, 3) -> Obj2.BoxR,
+        Vec(7, 3) -> Obj2.BoxL, Vec(8, 3) -> Obj2.BoxR,
+        Vec(6, 4) -> Obj2.BoxL, Vec(7, 4) -> Obj2.BoxR,
+      ))
+    )
+  }
+
+  test("solution2") {
+    assertEquals(solution2(parseInput(input0)), 102)
+    // assertEquals(solution2(parseInput(input1)), 2028)
+    assertEquals(solution2(parseInput(input2)), 9021)
+  }
 }
