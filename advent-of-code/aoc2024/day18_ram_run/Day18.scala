220596f8b08a90fe2827ad15cbea8b418c5629be
anton0xf <anton0xf@gmail.com>
2024-12-21 14:01:25 +0300

    aoc2024/day18_ram_run: part 1

--- advent-of-code/aoc2024/day18_ram_run/Day18.scala
+++ advent-of-code/aoc2024/day18_ram_run/Day18.scala
@@ -1,4 +1,6 @@
+import scala.annotation.{tailrec, targetName}
 import scala.collection.immutable
+import scala.collection.immutable.Queue
 import scala.util.Using
 import scala.io.Source
 
@@ -6,18 +8,76 @@ object Day18 {
   def main(args: Array[String]): Unit = {
     Using(Source.fromFile("day18_ram_run/input")) { source =>
       val input = parseInput(source.getLines())
-      println(s"part 1: ${solution1(input)}")
+      val corner = Vec(70, 70)
+      val mem1 = Mem(corner, input.take(1024).toSet)
+      println(s"part 1: ${solution1(mem1)}")
     }
   }
 
-  case class Input(lines: List[String])
+  type Input = List[Vec]
 
-  def parseInput(lines: IterableOnce[String]): Input = Input(lines.iterator.toList)
+  case class Vec(x: Int, y: Int) {
+    @targetName("plus")
+    def +(p: Vec): Vec = Vec(x + p.x, y + p.y)
+    def neighbours: Set[Vec] = dirs.map(this + _)
+  }
+
+  val dirs: Set[Vec] = Set(Vec(0, 1), Vec(1, 0), Vec(0, -1), Vec(-1, 0))
+
+  case class Mem(corner: Vec, bytes: Set[Vec]) {
+    def show: String = (0 to corner.y)
+      .map { y =>
+        (0 to corner.x).map { x =>
+          if bytes.contains(Vec(x, y)) then '#' else '.'
+        }.mkString
+      }
+      .mkString("\n")
+
+    def in(p: Vec): Boolean = (0 to corner.x).contains(p.x) && (0 to corner.y).contains(p.y)
+    def neighbours(p: Vec): Set[Vec] = p.neighbours.filter { p => in(p) && !bytes.contains(p) }
+  }
+
+  def parseInput(lines: IterableOnce[String]): Input = lines.iterator.map { line =>
+    line.split(',').map(_.toInt) match
+      case Array(x, y) => Vec(x, y)
+      case _           => throw new Exception(s"unexpected format: $line")
+  }.toList
 
   // part 1
-  def solution1(input: Input): Int = {
-    // println(input)
-    input.lines.size
+  def solution1(mem: Mem): Int = {
+    val p = findPath(mem)
+    p.get.size - 1
+  }
+
+  def findPath(mem: Mem): Option[List[Vec]] = {
+    val start = Vec(0, 0)
+    val end = mem.corner
+    @tailrec
+    def bfs(q: Queue[Vec], visited: Set[Vec], prev: Map[Vec, Vec]): Option[List[Vec]] = {
+      if q.isEmpty then None
+      else {
+        val (p, q1) = q.dequeue
+        if p == end then Some(backtrack(p, prev))
+        else if visited.contains(p) then bfs(q1, visited, prev)
+        else {
+          val ns = mem.neighbours(p)
+          bfs(
+            q1.enqueueAll(ns),
+            visited + p,
+            ns.foldLeft(prev) { (prev, n) => prev.updatedWith(n)(_.orElse(Some(p))) }
+          )
+        }
+      }
+    }
+    def backtrack(p: Vec, prev: Map[Vec, Vec]): List[Vec] = {
+      @tailrec
+      def go(acc: List[Vec]): List[Vec] = {
+        val p = acc.head
+        if p == start then acc else go(prev(p) :: acc)
+      }
+      go(p :: Nil)
+    }
+    bfs(Queue(start), Set(), Map())
   }
 
   // part 2
--- advent-of-code/aoc2024/day18_ram_run/Day18Test.scala
+++ advent-of-code/aoc2024/day18_ram_run/Day18Test.scala
@@ -4,16 +4,59 @@ import scala.jdk.StreamConverters.*
 
 class Day18Test extends munit.FunSuite {
   val input: List[String] =
-    """one
-      |two""".stripMargin.lines().toScala(List)
+    """5,4
+      |4,2
+      |4,5
+      |3,0
+      |2,1
+      |6,3
+      |2,4
+      |1,5
+      |0,6
+      |3,3
+      |2,6
+      |5,1
+      |1,2
+      |5,5
+      |2,5
+      |6,5
+      |1,4
+      |0,4
+      |6,4
+      |1,1
+      |6,1
+      |1,0
+      |0,5
+      |1,6
+      |2,0""".stripMargin.lines().toScala(List)
 
   test("parseInput") {
-    assertEquals(parseInput(input), Input(List("one", "two")))
+    assertEquals(parseInput("5,4\n2,0".lines().toScala(List)), List(Vec(5, 4), Vec(2, 0)))
+  }
+
+  private val testMem: Mem = Mem(Vec(6, 6), parseInput(input).take(12).toSet)
+
+  test("show") {
+    assertEquals(
+      testMem.show,
+      """...#...
+        |..#..#.
+        |....#..
+        |...#..#
+        |..#..#.
+        |.#..#..
+        |#.#....""".stripMargin
+    )
   }
 
   // part 1
-  test("solution1"){
-    assertEquals(solution1(parseInput(input)), 2)
+  test("solution1") {
+    assertEquals(solution1(testMem), 22)
+  }
+
+  test("findPath") {
+    val mem = Mem(Vec(1, 1), Set(Vec(0, 1)))
+    assertEquals(findPath(mem), Some(List(Vec(0, 0), Vec(1, 0), Vec(1, 1))))
   }
 
   // part 2
