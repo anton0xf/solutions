15bd04d53b2d2d8b86f48ac9248362ec33bb37af
anton0xf <anton0xf@gmail.com>
2024-12-30 16:33:46 +0300

    aoc2024/day21_keypad_conundrum: part 2

--- advent-of-code/aoc2024/day21_keypad_conundrum/Day21.scala
+++ advent-of-code/aoc2024/day21_keypad_conundrum/Day21.scala
@@ -1,5 +1,5 @@
 import scala.annotation.{tailrec, targetName}
-import scala.collection.immutable
+import scala.collection.{immutable, mutable}
 import scala.util.Using
 import scala.io.Source
 
@@ -8,6 +8,7 @@ object Day21 {
     Using(Source.fromFile("day21_keypad_conundrum/input")) { source =>
       val input = parseInput(source.getLines())
       println(s"part 1: ${solution1(input)}")
+      println(s"part 2: ${solution2(input)}")
     }
   }
 
@@ -80,28 +81,33 @@ object Day21 {
     length * num
   }
 
-  def typeIndirect(s: String): List[String] = {
+  def typeIndirect(s: String): LazyList[String] = {
     val progs1 = typeString(s, numpad)
     val progs2 = typeShortest(progs1, dirpad)
     typeShortest(progs2, dirpad)
   }
 
-  def typeShortest(progs: List[String], pad: Pad): List[String] = {
+  def typeShortest(progs: LazyList[String], pad: Pad): LazyList[String] = {
     val ps = progs.map(typeString(_, dirpad))
     val shortestSize = ps.map(_.head.length).min
     ps.filter(_.head.length == shortestSize).flatten
   }
 
-  def typeString(s: String, pad: Pad): List[String] = {
+  def typeString(s: String, pad: Pad): LazyList[String] = {
     @tailrec
-    def go(s: List[Char], p: Vec, acc: List[Vector[Char]]): List[String] = s match
+    def go(s: List[Char], p: Vec, acc: LazyList[Vector[Char]]): LazyList[String] = s match
       case Nil => acc.map(_.mkString)
       case ch :: s =>
-        val progs = typeChar(ch, p, pad)
+        val progs = typeCharM(ch, p, pad)
         go(s, pad.find(ch), acc.flatMap(pre => progs.map(pre ++ _)))
 
-    go(s.toList, pad.start, List(Vector()))
+    go(s.toList, pad.start, LazyList(Vector()))
   }
+
+  val typeCharCache: mutable.Map[(Char, Vec, Pad), List[Vector[Char]]] = mutable.Map()
+
+  def typeCharM(ch: Char, p: Vec, pad: Pad): List[Vector[Char]] =
+    typeCharCache.getOrElseUpdate((ch, p, pad), typeChar(ch, p, pad))
 
   def typeChar(ch: Char, p: Vec, pad: Pad): List[Vector[Char]] = {
     def moveJ(dj: Int) = Vector.fill(dj.abs)(if dj >= 0 then '>' else '<')
@@ -117,4 +123,44 @@ object Day21 {
   }
 
   // part 2
+  def solution2(input: Input): BigInt = {
+    input.map(complexity2(numpad :: List.fill(25)(dirpad))).sum
+  }
+
+  def complexity2(pads: List[Pad])(s: String): BigInt = {
+    val length = typeStringIndirect(pads, s)
+    val num = s.replace("A", "").toInt
+    length * num
+  }
+
+  def typeIndirect2(n: Int, s: String): Int = {
+    val progs = typeString(s, numpad)
+    @tailrec
+    def go(n: Int, progs: LazyList[String]): LazyList[String] =
+      if n == 0 then progs else go(n - 1, typeShortest(progs, dirpad))
+    go(n, progs).head.length
+  }
+
+  def typeStringIndirect(pads: List[Pad], s: String): BigInt = {
+    // println(s"type $s at level ${pads.size}")
+    @tailrec
+    def go(s: List[Char], p: Vec, acc: BigInt): BigInt = s match
+      case Nil     => acc
+      case ch :: s => go(s, pads.head.find(ch), acc + typeCharIndirectM(ch, p, pads))
+
+    go(s.toList, pads.head.start, 0)
+  }
+
+  val typeCharIndirectCache: mutable.Map[(Char, Vec, Int), BigInt] = mutable.Map()
+
+  def typeCharIndirectM(ch: Char, p: Vec, pads: List[Pad]): BigInt =
+    typeCharIndirectCache.getOrElseUpdate((ch, p, pads.size), typeCharIndirect(ch, p, pads))
+
+  def typeCharIndirect(ch: Char, p: Vec, pads: List[Pad]): BigInt = {
+    // println(s"type '$ch' from $p at level ${pads.size}")
+    pads match
+      case Nil         => 1
+      case pad :: Nil  => typeCharM(ch, p, pad).map(_.size).min
+      case pad :: pads => typeCharM(ch, p, pad).map(chs => typeStringIndirect(pads, chs.mkString)).min
+  }
 }
