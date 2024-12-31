01b1f5f462c7405b317b49e48b63898dfb4541b0
anton0xf <anton0xf@gmail.com>
2024-12-31 11:14:06 +0300

    aoc2024/day22_monkey_market: part 1

--- advent-of-code/aoc2024/day22_monkey_market/Day22.scala
+++ advent-of-code/aoc2024/day22_monkey_market/Day22.scala
@@ -6,19 +6,30 @@ object Day22 {
   def main(args: Array[String]): Unit = {
     Using(Source.fromFile("day22_monkey_market/input")) { source =>
       val input = parseInput(source.getLines())
+      println(s"input size: ${input.size}")
       println(s"part 1: ${solution1(input)}")
     }
   }
 
-  case class Input(lines: List[String])
+  type Input = List[BigInt]
 
-  def parseInput(lines: IterableOnce[String]): Input = Input(lines.iterator.toList)
+  def parseInput(lines: IterableOnce[String]): Input = lines.iterator.map(BigInt.apply).toList
 
   // part 1
-  def solution1(input: Input): Int = {
-    // println(input)
-    input.lines.size
+  def solution1(input: Input): BigInt = {
+    input.map(simulate).sum
   }
+
+  def simulate(s: BigInt): BigInt = LazyList.iterate(s)(next).drop(2000).head
+
+  def next(s: BigInt): BigInt = {
+    val s1 = prune(mix(s * 64, s))
+    val s2 = prune(mix(s1 / 32, s1))
+    prune(mix(s2 * 2048, s2))
+  }
+
+  def mix(n: BigInt, s: BigInt): BigInt = n ^ s
+  def prune(n: BigInt): BigInt = n % BigInt("16777216")
 
   // part 2
 }
--- advent-of-code/aoc2024/day22_monkey_market/Day22Test.scala
+++ advent-of-code/aoc2024/day22_monkey_market/Day22Test.scala
@@ -4,16 +4,42 @@ import scala.jdk.StreamConverters.*
 
 class Day22Test extends munit.FunSuite {
   val input: List[String] =
-    """one
-      |two""".stripMargin.lines().toScala(List)
+    """1
+      |10
+      |100
+      |2024""".stripMargin.lines().toScala(List)
 
   test("parseInput") {
-    assertEquals(parseInput(input), Input(List("one", "two")))
+    assertEquals(parseInput(input), List(1, 10, 100, 2024).map(BigInt.apply))
   }
 
   // part 1
-  test("solution1"){
-    assertEquals(solution1(parseInput(input)), 2)
+  test("solution1") {
+    assertEquals(solution1(parseInput(input)), BigInt("37327623"))
+  }
+
+  test("simulate") {
+    assertEquals(simulate(BigInt(1)), BigInt("8685429"))
+    assertEquals(simulate(BigInt(10)), BigInt("4700978"))
+    assertEquals(simulate(BigInt(100)), BigInt("15273692"))
+    assertEquals(simulate(BigInt(2024)), BigInt("8667524"))
+  }
+
+  test("next") {
+    assertEquals(next(BigInt(123)), BigInt(15887950))
+    assertEquals(
+      LazyList.iterate(BigInt(123))(next).tail.take(10).toList,
+      List(15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254)
+        .map(BigInt.apply)
+    )
+  }
+
+  test("mix") {
+    assertEquals(mix(BigInt(15), BigInt(42)), BigInt(37))
+  }
+
+  test("prune") {
+    assertEquals(prune(BigInt("100000000")), BigInt("16113920"))
   }
 
   // part 2
