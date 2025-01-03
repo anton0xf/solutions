e76603d8412613fc9bac247b98c59b46c50eb657
anton0xf <anton0xf@gmail.com>
2025-01-03 17:11:45 +0300

    aoc2024/day24_crossed_wires: init

---
+++ advent-of-code/aoc2024/day24_crossed_wires/Day24Test.scala
@@ -0,0 +1,20 @@
+//> using target.scope test
+import Day24.*
+import scala.jdk.StreamConverters.*
+
+class Day24Test extends munit.FunSuite {
+  val input: List[String] =
+    """one
+      |two""".stripMargin.lines().toScala(List)
+
+  test("parseInput") {
+    assertEquals(parseInput(input), Input(List("one", "two")))
+  }
+
+  // part 1
+  test("solution1"){
+    assertEquals(solution1(parseInput(input)), 2)
+  }
+
+  // part 2
+}
