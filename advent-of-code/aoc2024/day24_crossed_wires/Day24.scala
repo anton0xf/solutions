9759694fef96b3495d302c44924eedb1976938ea
anton0xf <anton0xf@gmail.com>
2025-01-04 14:36:24 +0300

    aoc2024/day24_crossed_wires: part 1

--- advent-of-code/aoc2024/day24_crossed_wires/Day24.scala
+++ advent-of-code/aoc2024/day24_crossed_wires/Day24.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.collection.immutable
 import scala.util.Using
 import scala.io.Source
@@ -10,14 +11,97 @@ object Day24 {
     }
   }
 
-  case class Input(lines: List[String])
+  case class Input(wires: Map[String, Boolean], gates: List[Gate])
+  case class Gate(op: Op, inA: String, inB: String, out: String)
+  sealed trait Op {
+    def eval(a: Boolean, b: Boolean): Boolean
+  }
 
-  def parseInput(lines: IterableOnce[String]): Input = Input(lines.iterator.toList)
+  object Op {
+    case object And extends Op:
+      def eval(a: Boolean, b: Boolean): Boolean = a && b
+
+    case object Or extends Op:
+      def eval(a: Boolean, b: Boolean): Boolean = a || b
+
+    case object Xor extends Op:
+      def eval(a: Boolean, b: Boolean): Boolean = a ^ b
+  }
+
+  def parseInput(lines: IterableOnce[String]): Input = {
+    val (wireLines, gateLines) = lines.iterator.dropWhile(_.isEmpty).span(_.nonEmpty)
+    Input(
+      wires = wireLines.map { line =>
+        line.split(":\\s+") match
+          case Array(wire, state) => wire -> toBool(state)
+          case _                  => throw new Exception(s"unexpected wire line: $line")
+      }.toMap,
+      gates = gateLines
+        .filterNot(_.isBlank)
+        .map { line =>
+          line.split(' ') match
+            case Array(a, op, b, "->", res) => Gate(toOp(op), a, b, res)
+            case _                          => throw new Exception(s"unexpected gate line: $line")
+        }
+        .toList
+    )
+  }
+
+  def toBool(s: String): Boolean = s match
+    case "0" => false
+    case "1" => true
+    case _   => throw new Exception(s"unexpected wire state: $s")
+
+  def toOp(s: String): Op = s match
+    case "AND" => Op.And
+    case "OR"  => Op.Or
+    case "XOR" => Op.Xor
 
   // part 1
-  def solution1(input: Input): Int = {
+  def solution1(input: Input): BigInt = {
     // println(input)
-    input.lines.size
+    val wires = simulate(input)
+    val bits = wires.iterator
+      .filter { (wire, _) => wire.startsWith("z") }
+      .map { (wire, state) => (wire.substring(1).toInt, state) }
+      .toVector
+      .sortBy(_._1)
+      .map(_._2)
+    toNum(bits)
+  }
+
+  def simulate(input: Input): Map[String, Boolean] = {
+    @tailrec
+    def go(
+        wires: Map[String, Boolean],
+        changed: Boolean,
+        gates1: List[Gate],
+        gates2: List[Gate]
+    ): Map[String, Boolean] = gates1 match {
+      case Nil => if changed && gates2.nonEmpty then go(wires, false, gates2, Nil) else wires
+      case gate :: gates1 =>
+        (for {
+          a <- wires.get(gate.inA)
+          b <- wires.get(gate.inB)
+        } yield gate.out -> gate.op.eval(a, b)) match {
+          case None                => go(wires, changed, gates1, gate :: gates2)
+          case Some(wire -> state) => go(wires.updated(wire, state), true, gates1, gates2)
+        }
+    }
+    go(input.wires, false, input.gates, Nil)
+  }
+
+  /** Get integer number from sequence of its bits from the least significant to most significant. <br\> For example
+    * <code>toNum(Seq(false, true, true)) == 6</code>
+    */
+  def toNum(bools: Seq[Boolean]): BigInt = {
+    @tailrec
+    def go(bools: Seq[Boolean], n: BigInt): BigInt = bools match {
+      case Seq()          => n
+      case false +: bools => go(bools, n * 2)
+      case true +: bools  => go(bools, n * 2 + 1)
+    }
+    go(bools.reverse, 0)
   }
 
   // part 2
--- advent-of-code/aoc2024/day24_crossed_wires/Day24Test.scala
+++ advent-of-code/aoc2024/day24_crossed_wires/Day24Test.scala
@@ -3,17 +3,97 @@ import Day24.*
 import scala.jdk.StreamConverters.*
 
 class Day24Test extends munit.FunSuite {
-  val input: List[String] =
-    """one
-      |two""".stripMargin.lines().toScala(List)
+  val input0: List[String] =
+    """x00: 1
+      |y00: 0
+      |
+      |x00 AND y00 -> z00
+      |""".stripMargin.lines().toScala(List)
+
+  val input1: List[String] =
+    """x00: 1
+      |x01: 1
+      |x02: 1
+      |y00: 0
+      |y01: 1
+      |y02: 0
+      |
+      |x00 AND y00 -> z00
+      |x01 XOR y01 -> z01
+      |x02 OR y02 -> z02""".stripMargin.lines().toScala(List)
+
+  val input2: List[String] =
+    """
+      |
+      |x00: 1
+      |x01: 0
+      |x02: 1
+      |x03: 1
+      |x04: 0
+      |y00: 1
+      |y01: 1
+      |y02: 1
+      |y03: 1
+      |y04: 1
+      |
+      |ntg XOR fgs -> mjb
+      |y02 OR x01 -> tnw
+      |kwq OR kpj -> z05
+      |x00 OR x03 -> fst
+      |tgd XOR rvg -> z01
+      |vdt OR tnw -> bfw
+      |bfw AND frj -> z10
+      |ffh OR nrd -> bqk
+      |y00 AND y03 -> djm
+      |y03 OR y00 -> psh
+      |bqk OR frj -> z08
+      |tnw OR fst -> frj
+      |gnj AND tgd -> z11
+      |bfw XOR mjb -> z00
+      |x03 OR x00 -> vdt
+      |gnj AND wpb -> z02
+      |x04 AND y00 -> kjc
+      |djm OR pbm -> qhw
+      |nrd AND vdt -> hwm
+      |kjc AND fst -> rvg
+      |y04 OR y02 -> fgs
+      |y01 AND x02 -> pbm
+      |ntg OR kjc -> kwq
+      |psh XOR fgs -> tgd
+      |qhw XOR tgd -> z09
+      |pbm OR djm -> kpj
+      |x03 XOR y03 -> ffh
+      |x00 XOR y04 -> ntg
+      |bfw OR bqk -> z06
+      |nrd XOR fgs -> wpb
+      |frj XOR qhw -> z04
+      |bqk OR frj -> z07
+      |y03 OR x01 -> nrd
+      |hwm AND bqk -> z03
+      |tgd XOR rvg -> z12
+      |tnw OR pbm -> gnj
+      |""".stripMargin.lines().toScala(List)
 
   test("parseInput") {
-    assertEquals(parseInput(input), Input(List("one", "two")))
+    assertEquals(parseInput(input0), Input(
+      Map("x00" -> true, "y00" -> false),
+      List(Gate(Op.And, "x00", "y00", "z00"))
+    ))
   }
 
   // part 1
   test("solution1"){
-    assertEquals(solution1(parseInput(input)), 2)
+    assertEquals(solution1(parseInput(input0)).toInt, 0)
+    assertEquals(solution1(parseInput(input1)).toInt, 4)
+    assertEquals(solution1(parseInput(input2)).toInt, 2024)
+  }
+
+
+  test("toNum") {
+    assertEquals(toNum(Seq(false, true, true)).toInt, 6)
+    assertEquals(toNum(Seq(false, true, false, true)).toInt, 10)
+    assertEquals(toNum(Seq(false, true, false, true, false)).toInt, 10)
+    assertEquals(toNum(Seq(true, false, true, false)).toInt, 5)
   }
 
   // part 2
