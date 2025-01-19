c4ea0d4e5939a324973b50216877a902b527d3c1
anton0xf <anton0xf@gmail.com>
2025-01-19 17:05:43 +0300

    exercism/clojure/log-levels: done

--- exercism/clojure/log-levels/src/log_levels.clj
+++ exercism/clojure/log-levels/src/log_levels.clj
@@ -1,20 +1,26 @@
 (ns log-levels
   (:require [clojure.string :as str]))
 
+(defn parse-log [s]
+  (let [[_ level message] (re-matches #"(?sm)^\[(\w+)\]:\s*(.*)$" s)]
+    {:level (str/lower-case level)
+     :message (str/trimr message)}))
+
 (defn message
   "Takes a string representing a log line
    and returns its message with whitespace trimmed."
   [s]
-  )
+  (:message (parse-log s)))
 
 (defn log-level
   "Takes a string representing a log line
    and returns its level in lower-case."
   [s]
-  )
+  (:level (parse-log s)))
 
 (defn reformat
   "Takes a string representing a log line and formats it
    with the message first and the log level in parentheses."
   [s]
-  )
+  (let [{:keys [level message]} (parse-log s)]
+    (format "%s (%s)" message level)))
