6252e141b7f28c53643c71527f79af95a5236a38
anton0xf <anton0xf@gmail.com>
2025-01-19 14:17:11 +0300

    exercism/clojure/robot-simulator: done

--- exercism/clojure/robot-simulator/src/robot_simulator.clj
+++ exercism/clojure/robot-simulator/src/robot_simulator.clj
@@ -1,11 +1,32 @@
 (ns robot-simulator)
 
-(defn robot
-  [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn robot [pos bearing]
+  {:coordinates pos, :bearing bearing})
 
-(defn simulate
-  [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(def bearings '(:north :east :south :west))
+
+(def bearings-right
+  (zipmap bearings (rest (cycle bearings))))
+
+(def bearings-left
+  (zipmap (rest (cycle bearings)) bearings))
+
+(defn pos-add [p1 p2] {:x (+ (:x p1) (:x p2))
+                       :y (+ (:y p1) (:y p2))})
+
+(defn pos-rotate-right [{:keys [x y]}]
+  {:x y, :y (- x)})
+
+(def move-north {:x 0 :y 1})
+
+(def moves "map from bearing to move vector"
+  (zipmap bearings (iterate pos-rotate-right move-north)))
+
+(defn step [robot cmd]
+  (case cmd 
+    \R (update robot :bearing bearings-right)
+    \L (update robot :bearing bearings-left)
+    \A (update robot :coordinates #(pos-add % (moves (:bearing robot))))))
+
+(defn simulate [cmds robot]
+  (reduce step robot cmds))
