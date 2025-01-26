441d17212520a02c4bf95c72b69c91378b01430c
anton0xf <anton0xf@gmail.com>
2025-01-26 22:55:06 +0300

    exercism/jq/bird-watcher: done

--- exercism/clojure/bird-watcher/src/bird_watcher.clj
+++ exercism/clojure/bird-watcher/src/bird_watcher.clj
@@ -1,22 +1,27 @@
 (ns bird-watcher)
 
 (def last-week 
-  )
+  [0 2 5 3 7 8 4])
 
 (defn today [birds]
-  )
+  (peek birds))
 
 (defn inc-bird [birds]
-  )
+  (conj (pop birds) (inc (today birds))))
 
 (defn day-without-birds? [birds]
-  )
+  (true? (some zero? birds)))
 
 (defn n-days-count [birds n]
-  )
+  (reduce + 0 (take n birds)))
 
 (defn busy-days [birds]
-  )
+  (count (filter #(>= % 5) birds)))
 
 (defn odd-week? [birds]
-  )
+  (loop [x (first birds)
+         xs (rest birds)]
+    (cond
+      (not (contains? #{0 1} x)) false
+      (empty? xs) true
+      (not= x (first xs)) (recur (first xs) (rest xs)))))
