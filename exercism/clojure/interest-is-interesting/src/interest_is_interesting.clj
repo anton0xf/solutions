cf869b8878fa294bf8deb3008d1583892eecc678
anton0xf <anton0xf@gmail.com>
2025-01-28 08:43:37 +0300

    exercism/jq/interest-is-interesting: done

--- exercism/clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ exercism/clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -3,14 +3,24 @@
 (defn interest-rate
   "Returns the interest rate based on the specified balance."
   [balance]
-  )
+  (cond
+    (neg? balance) -3.213
+    (< balance 1000) 0.5
+    (< balance 5000) 1.621
+    :else 2.475))
 
 (defn annual-balance-update
   "Returns the annual balance update, taking into account the interest rate."
   [balance]
-  )
+  (+ balance (* (abs balance)
+                (bigdec (/ (interest-rate balance) 100)))))
+
+(defn round-down [^BigDecimal n]
+  (.intValue (.setScale n 0 java.math.RoundingMode/DOWN)))
 
 (defn amount-to-donate
   "Returns how much money to donate based on the balance and the tax-free percentage."
   [balance tax-free-percentage]
-  )
+  (if (neg? balance) 0
+      (round-down (* balance (bigdec (/ tax-free-percentage 100)) 2))))
+
