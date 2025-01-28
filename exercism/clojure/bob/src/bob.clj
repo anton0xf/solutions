b80aa7a4e9e5b6e1f2f50dda0229225a5564ca9b
anton0xf <anton0xf@gmail.com>
2025-01-28 09:42:50 +0300

    exercism/jq/bob: done

--- exercism/clojure/bob/src/bob.clj
+++ exercism/clojure/bob/src/bob.clj
@@ -1,5 +1,22 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string :as str]))
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn question? [s]
+  (str/ends-with? (str/trimr s) "?"))
+
+(defn yell?
+  "s is in upper case and contains at least one letter"
+  [s]
+  (and (= s (str/upper-case s))
+       (re-find #"[A-Z]" s)))
+
+(defn silence? [s]
+  (str/blank? s))
+
+(defn response-for [s]
+  (cond
+    (and (question? s) (yell? s)) "Calm down, I know what I'm doing!"
+    (question? s) "Sure."
+    (yell? s) "Whoa, chill out!"
+    (silence? s) "Fine. Be that way!"
+    :else "Whatever."))
