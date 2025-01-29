fc2cda89378ab44b046439cd5369386fb4752974
anton0xf <anton0xf@gmail.com>
2025-01-29 09:10:55 +0300

    exercism/clojure/raindrops: done

--- exercism/clojure/raindrops/src/raindrops.clj
+++ exercism/clojure/raindrops/src/raindrops.clj
@@ -1,5 +1,14 @@
-(ns raindrops)
+(ns raindrops
+  (:require [clojure.string :as str]))
 
-(defn convert [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn div?
+  "is n divisible by div?"
+  [n div]
+  (zero? (rem n div)))
+
+(defn convert [n]
+  (let [s (str/join [(if (div? n 3) "Pling" "")
+                     (if (div? n 5) "Plang" "")
+                     (if (div? n 7) "Plong" "")])]
+    (if (= s "") (str n) s)))
+
