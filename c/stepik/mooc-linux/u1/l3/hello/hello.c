db22b0f75a6f034ba306406a20f89eb666384c7e
anton0xf <anton0xf@gmail.com>
2025-01-29 09:50:39 +0300

    mooc-linux: 1.3.2. hello dlopen() - use dlclose()

--- c/stepik/mooc-linux/u1/l3/hello/hello.c
+++ c/stepik/mooc-linux/u1/l3/hello/hello.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
-void hello_message(const char* name) {
+void hello_message(const char *name) {
   printf("Hello, %s!\n", name);
 }
--- c/stepik/mooc-linux/u1/l3/hello/main.c
+++ c/stepik/mooc-linux/u1/l3/hello/main.c
@@ -5,25 +5,31 @@
 
 void (*hello_message)(const char*);
 
-bool init_library() {
+void* init_library() {
   void *hdl = dlopen("./libhello.so", RTLD_LAZY);
   if (NULL == hdl) {
-    return false;
+    fprintf(stderr, "%s\n", dlerror());
+    return NULL;
   }
+  dlerror();    /* Clear any existing error */
 
   hello_message = (void (*)(const char*)) dlsym(hdl, "hello_message");
-  if (NULL == hello_message) {
-    return false;
+  char *error = dlerror();
+  if (error != NULL) {
+    fprintf(stderr, "%s\n", error);
+    return NULL;
   }
 
-  return true;
+  return hdl;
 }
 
 int main() {
-  if (init_library()) {
+  void *hdl = init_library();
+  if (NULL != hdl) {
     hello_message("Vasya");
   } else {
     printf("Library was not loaded\n");
   }
+  dlclose(hdl);
   return 0;
 }
