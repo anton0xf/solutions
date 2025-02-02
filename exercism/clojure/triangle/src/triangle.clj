aa167479566b7cc4c6f10614fa6be9618749861a
anton0xf <anton0xf@gmail.com>
2025-02-02 14:57:43 +0300

    exercism/clojure/triangle: init

---
+++ exercism/clojure/triangle/src/triangle.clj
@@ -0,0 +1,19 @@
+(ns triangle)
+
+(defn equilateral?
+  "Returns true if the triangle with sides a, b, and c is equilateral; otherwise, returns false"
+  [a b c]
+  ;; function body
+  )
+
+(defn isosceles?
+  "Returns true if the triangle with sides a, b, and c is isosceles; otherwise, returns false"
+  [a b c]
+  ;; function body
+  )
+
+(defn scalene?
+  "Returns true if the triangle with sides a, b, and c is scalene; otherwise, returns false"
+  [a b c]
+  ;; function body
+  )
