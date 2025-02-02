24248859b5d9fee638021c251b9a53525682b03c
anton0xf <anton0xf@gmail.com>
2025-02-02 15:46:46 +0300

    exercism/clojure/triangle: done

--- exercism/clojure/triangle/src/triangle.clj
+++ exercism/clojure/triangle/src/triangle.clj
@@ -1,19 +1,26 @@
 (ns triangle)
 
+(defn triangle? [a b c]
+  (and (pos? a) (pos? b) (pos? c)
+       (<= a (+ b c))
+       (<= b (+ a c))
+       (<= c (+ a b))))
+
 (defn equilateral?
   "Returns true if the triangle with sides a, b, and c is equilateral; otherwise, returns false"
   [a b c]
-  ;; function body
-  )
+  (and (pos? a) (= a b c)))
 
 (defn isosceles?
   "Returns true if the triangle with sides a, b, and c is isosceles; otherwise, returns false"
   [a b c]
-  ;; function body
-  )
+  (and (triangle? a b c)
+       (or (= a b)
+           (= a c)
+           (= b c))))
 
 (defn scalene?
   "Returns true if the triangle with sides a, b, and c is scalene; otherwise, returns false"
   [a b c]
-  ;; function body
-  )
+  (and (triangle? a b c)
+       (not (isosceles? a b c))))
