1ea0f3065a15477fb79dfc96e640cd9417e87a76
anton0xf <anton0xf@gmail.com>
2025-02-04 20:20:22 +0300

    exercism/go/weather-forecast: done

--- exercism/go/weather-forecast/weather_forecast.go
+++ exercism/go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather forecast the current weather condition of various cities in Goblinocus.
 package weather
 
+// CurrentCondition represents current weather condition.
 var CurrentCondition string
+
+// CurrentLocation represents current city.
 var CurrentLocation string
 
+// Forecast returns a string representation of weather forecast.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
