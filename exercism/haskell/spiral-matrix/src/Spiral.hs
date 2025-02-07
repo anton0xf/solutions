4240dfb083a5f67eeb8ec8e0a5ec1ed48a24dbb5
anton0xf <anton0xf@gmail.com>
2025-02-07 10:44:13 +0300

    exercism/haskell/spiral-matrix: simplify

--- exercism/haskell/spiral-matrix/src/Spiral.hs
+++ exercism/haskell/spiral-matrix/src/Spiral.hs
@@ -2,26 +2,22 @@ module Spiral (spiral) where
 
 import Data.Function ((&))
 import Data.List (find)
-import Data.Maybe (fromJust)
+import Data.Maybe (isJust, fromJust)
 
 spiral :: Int -> [[Int]]
-spiral size = foldl go s0 [1 .. (size * size)] & m & toLists size
-  where s0 = State {
+spiral size = foldl go st [1 .. (size * size)] & m & toLists size
+  where st = State {
           m = []
+          , sz = size
           , pos = (0, -1)
           , dir = (0, 1)
-          , steps = size
-          , step = 0
-          , rots = 1
           }
 
 data State = State {
   m :: Matrix
+  , sz :: Int -- size
   , pos :: Vec  -- current position
   , dir :: Vec -- direction
-  , steps :: Int -- count of steps in a given direction
-  , step :: Int -- current step in a given direction
-  , rots :: Int -- roots until steps decreasing
   } deriving Show
 
 type Vec = (Int, Int) -- (row, col), 0-based
@@ -32,38 +28,37 @@ plus (i1, j1) (i2, j2) = (i1 + i2, j1 + j2)
 rotate :: Vec -> Vec
 rotate (i, j) = (j, -i)
 
+inBounds :: Vec -> Vec -> Bool
+inBounds (i, j) (isz, jsz) = 0 <= i && i < isz
+  && 0 <= j && j < jsz
+
 type Matrix = [(Vec, Int)]
 
-matrixGet :: Vec -> Matrix -> Int
-matrixGet p = snd . fromJust . find ((== p) . fst)
+matrixGet :: Vec -> Matrix -> Maybe Int
+matrixGet p = fmap snd . find ((== p) . fst)
 
 go :: State -> Int -> State
-go s n = stepState n $ if shouldRotate s then rotateState s else s
+go st n =
+  let newSt = if shouldRotate st
+             then st {dir = rotate (st & dir)}
+             else st
+      newPos = nextPos newSt
+  in newSt {
+    pos = newPos
+    , m = (newPos, n) : (st & m)
+    }
 
 shouldRotate :: State -> Bool
-shouldRotate s = (s & step) == (s & steps)
+shouldRotate st =
+  let newPos = nextPos st
+      size = st & sz
+  in not (inBounds newPos (size, size)) || isJust (matrixGet newPos (st & m))
 
-rotateState :: State -> State
-rotateState s =
-  let newRots = (s & rots) - 1
-  in s {
-    dir = rotate (s & dir)
-    , step = 0
-    , steps = if newRots == 0 then (s & steps) - 1 else s & steps
-    , rots = if newRots == 0 then 2 else newRots
-    }
-
-stepState :: Int -> State -> State
-stepState n s =
-  let newPos = plus (s & pos) (s & dir)
-  in s {
-    step = step s + 1
-    , pos = newPos
-    , m = (newPos, n) : (s & m)
-    }
+nextPos :: State -> Vec
+nextPos st = plus (st & pos) (st & dir)
 
 toLists :: Int -> Matrix -> [[Int]]
 toLists size mx = do
   i <- [0 .. (size-1)]
-  pure [ matrixGet (i, j) mx | j <- [0 .. (size-1)] ]
+  pure [ matrixGet (i, j) mx & fromJust | j <- [0 .. (size-1)] ]
   
