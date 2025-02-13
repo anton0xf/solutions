2be0ac964f228a25340813ae16032489d5c9f96a
anton0xf <anton0xf@gmail.com>
2025-02-13 18:02:00 +0300

    exercism/go/animal-magic: done

--- exercism/go/animal-magic/animal_magic.go
+++ exercism/go/animal-magic/animal_magic.go
@@ -1,16 +1,34 @@
 package chance
 
+import "math/rand"
+
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	return rand.Intn(20) + 1
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64() * 12
+}
+
+var animals = []string{
+	"ant",
+	"beaver",
+	"cat",
+	"dog",
+	"elephant",
+	"fox",
+	"giraffe",
+	"hedgehog",
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	res := make([]string, len(animals))
+	copy(res, animals)
+	rand.Shuffle(len(res), func(i int, j int) {
+		res[i], res[j] = res[j], res[i]
+	})
+	return res
 }
