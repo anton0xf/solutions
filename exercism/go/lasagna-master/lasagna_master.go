3a85bc2f36812b04b8e8ea721204dfa54b69852d
anton0xf <anton0xf@gmail.com>
2025-02-15 23:31:12 +0300

    exercism/go/lasagna-master: done

--- exercism/go/lasagna-master/lasagna_master.go
+++ exercism/go/lasagna-master/lasagna_master.go
@@ -1,18 +1,33 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, time int) int {
+	if time == 0 {
+		time = 2
+	}
+	return len(layers) * time
+}
 
-// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (noodles int, sauce float64) {
+	for _, layer := range layers {
+		switch layer {
+		case "noodles":
+			noodles += 50
+		case "sauce":
+			sauce += 0.2
+		}
+	}
+	return
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friends, my []string) {
+	my[len(my)-1] = friends[len(friends)-1]
+}
 
-// TODO: define the 'ScaleRecipe()' function
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-// 
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
-// functionality.
+func ScaleRecipe(quantities []float64, n int) []float64 {
+	res := make([]float64, len(quantities))
+	scale := float64(n) / 2
+	for i, q := range quantities {
+		res[i] = q * scale
+	}
+	return res
+}
