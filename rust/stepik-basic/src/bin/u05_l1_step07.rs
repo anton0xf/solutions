8378ba93d4520e71d0e9bad8fc4b9a5c712ccc5b
anton0xf <anton0xf@gmail.com>
2025-05-11 23:14:13 +0300

    rust/stepik-basic: 5.1.7: tup 5

---
+++ rust/stepik-basic/src/bin/u05_l1_step07.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+// test it with: $ printf "%s\n" a b c d e | cargo run --bin u05_l1_step07
+fn main() {
+    let ls: Vec<_> = io::stdin().lines()
+        .filter_map(|line| line.ok())
+        .map(|line| line + "\n")
+        .collect();
+    let tup = (ls[0].as_str(), ls[1].as_str(), ls[2].as_str(), ls[3].as_str(), ls.get(4).unwrap());
+    println!("{tup:?}");
+}
