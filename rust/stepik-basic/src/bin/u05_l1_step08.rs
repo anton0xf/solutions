9ceb15985d614dad64a36bc56deaecdee718e1c3
anton0xf <anton0xf@gmail.com>
2025-05-12 00:11:08 +0300

    rust/stepik-basic: 5.1.8: corrector

---
+++ rust/stepik-basic/src/bin/u05_l1_step08.rs
@@ -0,0 +1,9 @@
+fn main() {
+    std::io::stdin().lines()
+        .filter_map(Result::ok)
+        .for_each(|s| {
+            let x = s.trim().parse::<f64>().expect("parse err") as i32;
+            print!("{x}, ")
+        });
+    println!("0");
+}
