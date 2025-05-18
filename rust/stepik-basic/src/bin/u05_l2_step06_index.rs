b538752b2876c1ae75b81ab3c2e1a4ce26da1aea
anton0xf <anton0xf@gmail.com>
2025-05-18 14:58:09 +0300

    rust/stepik-basic: 5.2.6: index (with from_fn)

--- rust/stepik-basic/src/bin/u05_l2_step06_index.rs
+++ rust/stepik-basic/src/bin/u05_l2_step06_index.rs
@@ -2,10 +2,10 @@
 fn main() {
     let lines: Vec<_> = std::io::stdin().lines()
         .filter_map(Result::ok).collect();
-    let xs: [f64; 5] = lines.iter().take(5)
+    let xs_vec: Vec<_> = lines.iter().take(5)
         .map(|s| s.parse::<f64>().unwrap())
-        .collect::<Vec<_>>()
-        .try_into().unwrap();
+        .collect::<Vec<_>>();
+    let xs: [f64; 5] = std::array::from_fn(|i| xs_vec[i]);
     let i: usize = lines.last().unwrap().parse().unwrap();
     println!("{:.2}", xs[i]);
 }
