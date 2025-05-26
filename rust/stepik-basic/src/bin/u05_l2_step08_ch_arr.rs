40e009705c837584377d0be52e0cf3e42e04f571
anton0xf <anton0xf@gmail.com>
2025-05-26 14:46:52 +0300

    rust/stepic-basic: 5.2.8. change array

---
+++ rust/stepik-basic/src/bin/u05_l2_step08_ch_arr.rs
@@ -0,0 +1,15 @@
+// test: $ printf '%s\n' 1 -10 | cargo run --bin u05_l2_step08_ch_arr
+// > [0, -10, 0, 0, 0, 0, 0, 0, 0, 0]
+fn main() {
+    let i_x: Vec<i32> = std::io::stdin().lines()
+        .map(|s| s.unwrap().trim().parse().unwrap())
+        .collect();
+    match i_x[..] {
+        [i, x] => {
+            let mut xs = [0; 10];
+            xs[i as usize] = x;
+            println!("{xs:?}");
+        }
+        _ => println!("error")
+    }
+}
