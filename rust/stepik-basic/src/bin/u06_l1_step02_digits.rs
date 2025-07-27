a003bb33ef7c04a1c838a933a84e3900bfcd040a
anton0xf <anton0xf@gmail.com>
2025-07-28 02:07:37 +0300

    rust/stepic-basic: 6.1.2. print two digits

---
+++ rust/stepik-basic/src/bin/u06_l1_step02_digits.rs
@@ -0,0 +1,7 @@
+// test: $ echo 21 | cargo run --bin u06_l1_step02_digits
+fn main() {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).expect("read err");
+    let n: u8 = s.trim().parse().expect("parse err");
+    println!("{}\n{}", n / 10, n % 10);
+}
