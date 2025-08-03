17e91178e13e421c46352c8aefe153d83f13d32d
anton0xf <anton0xf@gmail.com>
2025-08-03 20:43:56 +0300

    sicp/go-scheme/gs: Eval define(): add stub Eval() function

--- sicp/go-scheme/internal/sexp/eval.go
+++ sicp/go-scheme/internal/sexp/eval.go
@@ -1,5 +1,16 @@
 package sexp
 
-func Eval(e Expr) (Expr, error) {
-	return e, nil
+func Eval(expr Expr) (Expr, error) {
+	switch e := expr.(type) {
+	case *List:
+		return EvalList(e)
+
+	default:
+		return e, nil
+	}
+}
+
+func EvalList(e *List) (Expr, error) {
+	// if e == nil {
+	return nil, nil
 }
