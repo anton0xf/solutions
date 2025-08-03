24c54a1d2f7b903d24b8ecd890409db8fd69a796
anton0xf <anton0xf@gmail.com>
2025-08-03 20:45:00 +0300

    sicp/go-scheme/gs: Eval define(): main tests

--- sicp/go-scheme/cmd/gs/main_test.go
+++ sicp/go-scheme/cmd/gs/main_test.go
@@ -12,11 +12,11 @@ func TestRun(t *testing.T) {
 	cases := []struct {
 		in, out string
 	}{
-		{"fф -23", "fф\n-23\n"},
 		{"12 ", "12\n"},
+		{"'fф -23", "(quot fф)\n-23\n"},
 		{`"1a3"`, "\"1a3\"\n"},
-		{"a () (a 1\n()(2) )", "a\n()\n(a 1 () (2))\n"},
-		{"'10 'a", "10\n(quot a)\n"},
+		{"(define a 7) a () (a 1\n()(2) )", "(quot a)\n7\n()\n(7 1 () (2))\n"},
+		{"(define a 1) 'a a", "(quot a)\n(quot a)\n1\n"},
 	}
 	for _, c := range cases {
 		t.Run(c.in, func(t *testing.T) {
