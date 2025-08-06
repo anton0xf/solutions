253f103dbc97c84404b377c87a345fdc42448a43
anton0xf <anton0xf@gmail.com>
2025-08-06 03:08:53 +0300

    rust/stepic-basic: 6.1.4. sum digits - with recursion

--- rust/stepik-basic/src/bin/u06_l1_step04_sum_digits.rs
+++ rust/stepik-basic/src/bin/u06_l1_step04_sum_digits.rs
@@ -4,20 +4,9 @@ fn main() {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).expect("read err");
     let n: u16 = s.trim().parse().expect("parse err");
-    let sum: u16 = digits(n).iter().sum();
-    println!("{sum}");
+    println!("{}", sum_digits(n));
 }
 
-const BASE: u16 = 10;
-
-fn digits(n: u16) -> Vec<u16> {
-    if n == 0 {
-        vec![0]
-    } else if n < BASE {
-        vec![n]
-    } else {
-        let mut res = digits(n / BASE);
-        res.push(n % BASE);
-        res
-    }
+fn sum_digits(n: u16) -> u16 {
+    if n < 10 { n } else { sum_digits(n / 10) + n % 10 }
 }
