b743d49b14f61320575281c75bac0bf8aa8e776d
anton0xf <anton0xf@gmail.com>
2025-08-10 03:28:30 +0300

    sicp/go-scheme/gs: more Env.Eval() tests. quoted literals support

--- sicp/go-scheme/internal/sexp/env.go
+++ sicp/go-scheme/internal/sexp/env.go
@@ -4,8 +4,22 @@ type Env struct{}
 
 func (env *Env) Eval(expr Expr) (Expr, error) {
 	switch e := expr.(type) {
+	case *Quoted:
+		return EvalQuoted(e)
+
 	case *List:
 		return EvalList(e)
+
+	default:
+		return e, nil
+	}
+}
+
+func EvalQuoted(e *Quoted) (Expr, error) {
+	// TODO check e and e.x for nil
+	switch x := e.x.(type) {
+	case *Int, *String:
+		return x, nil
 
 	default:
 		return e, nil
---
+++ sicp/go-scheme/internal/sexp/env_test.go
@@ -0,0 +1,55 @@
+package sexp
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Str(s string) *string {
+	return &s
+}
+
+func TestEnv_Eval(t *testing.T) {
+	examples := []struct {
+		env      *Env
+		expr     Expr
+		envAfter *Env
+		result   Expr
+		err      string
+	}{
+		// literals are self-contained
+		{&Env{}, &Int{7}, &Env{}, &Int{7}, ""},
+		{&Env{}, &String{"aa"}, &Env{}, &String{"aa"}, ""},
+		// quoted literal is just literal
+		{&Env{}, &Quoted{&Int{3}}, &Env{}, &Int{3}, ""},
+		{&Env{}, &Quoted{&String{"aa"}}, &Env{}, &String{"aa"}, ""},
+		// other quotations don't change
+		{&Env{}, &Quoted{&Symbol{"x"}}, &Env{}, &Quoted{&Symbol{"x"}}, ""},
+		{&Env{}, &Quoted{&List{[]Expr{&Int{1}}}},
+			&Env{}, &Quoted{&List{[]Expr{&Int{1}}}}, ""},
+
+		// TODO Symbol (defined or undefined)
+
+		// List
+		// TODO error on empty list
+		// TODO call function
+
+		// special forms
+
+		// TODO define
+	}
+	for _, ex := range examples {
+		t.Run(fmt.Sprintf("[%v] %v", ex.env, ex.expr), func(t *testing.T) {
+			res, err := ex.env.Eval(ex.expr)
+			if len(ex.err) > 0 {
+				assert.EqualError(t, err, ex.err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, ex.result, res)
+			assert.Equal(t, ex.envAfter, ex.env)
+		})
+	}
+}
