a88b6f0678797511ca95c8c45f2088e11765cbf0
anton0xf <anton0xf@gmail.com>
2025-09-08 15:02:30 +0300

    set/cunningham: 1.2. Logical Notation: use NaryFunctions to write tautology and contradiction definitions

--- set/cunningham/1_intro/2_Logic.v
+++ set/cunningham/1_intro/2_Logic.v
@@ -3,11 +3,14 @@ Require Import Setoid.
 From Stdlib Require Import Classical.
 From Stdlib Require Import List.
 Import ListNotations.
+Open Scope bool_scope.
+Require Import Nat.
+From Stdlib Require Import NaryFunctions.
 
 Definition bvals := [true; false].
 
 (* Problem 1. Construct a truth table for the sentence ¬ P → (Q ∧ P) *)
-Definition pr1 (p q: bool) := implb (negb p) (andb q p).
+Definition pr1 (p q: bool) := implb (negb p) (q && p).
 
 Compute map (fun '(p, q) => (p, q, pr1 p q)) (list_prod bvals bvals).
 (* = [(true,  true,  true);
@@ -16,3 +19,26 @@ Compute map (fun '(p, q) => (p, q, pr1 p q)) (list_prod bvals bvals).
       (false, false, false)] *)
 Example pr1_is_p (p q: bool): pr1 p q = p.
 Proof. destruct p, q; reflexivity. Qed.
+
+(* use NaryFunctions *)
+Check (bool^^3 --> bool).
+Definition ConstB (b: bool) (n: nat) (f: bool^^n --> bool): Prop :=
+  forall x: bool^n, nuncurry bool bool n f x = b.
+
+(** Definition 1.2.1. *)
+Definition TautologyB := ConstB true.
+
+Theorem LEMB: TautologyB 1 (fun p => p || negb p).
+Proof.
+  unfold TautologyB, ConstB. destruct x. simpl.
+  destruct b; reflexivity.
+Qed.
+  
+(** Definition 1.2.2. *)
+Definition ContradictionB := ConstB false.
+
+Example ContradictionB__ex: ContradictionB 1 (fun p => p && negb p).
+Proof.
+  unfold ContradictionB, ConstB. destruct x. simpl.
+  destruct b; reflexivity.
+Qed.
