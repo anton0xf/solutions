8ce2510488b615b7ce08cf00483214f42260a38f
anton0xf <anton0xf@gmail.com>
2025-11-10 00:34:45 +0300

    rust/stepic-basic: 8.1.3. decode - optimize compact version

--- rust/stepik-basic/src/bin/u08_l1_step03_code_compact.rs
+++ rust/stepik-basic/src/bin/u08_l1_step03_code_compact.rs
@@ -1,9 +1,10 @@
 // test: $ echo '2134' | cargo run --bin u08_l1_step03_code_compact
 // > НОГИ
 fn main() {
+    let key: Vec<char> = "ОНГИ".chars().collect();
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).expect("read err");
     println!("{}", s.trim().chars()
-             .map(|d| "ОНГИ".chars().nth(d as usize - '1' as usize).expect("not found"))
+             .map(|d| key[d as usize - '1' as usize])
              .collect::<String>());
 }
