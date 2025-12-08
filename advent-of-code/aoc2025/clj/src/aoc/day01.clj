23f2cafdae900fa53e5b70b88923117860b0dc6c
anton0xf <anton0xf@gmail.com>
2025-12-08 16:04:23 +0300

    aoc2025: day01: part 2

--- advent-of-code/aoc2025/clj/src/aoc/day01.clj
+++ advent-of-code/aoc2025/clj/src/aoc/day01.clj
@@ -1,40 +1,67 @@
 (ns aoc.day01
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.math :refer [floor-div]]))
 
 (defn parse [s]
   (->> (str/split-lines s)
        (map (fn [s] [(.substring s 0 1)
                      (Integer/parseInt (.substring s 1))]))))
 
+(defn ceil-div ^long [^long x ^long y]
+  (java.lang.Math/ceilDiv x y))
+
+(defn range-len [a b]
+  (if (> a b) 0
+      (- (inc b) a)))
+
+(defn multiples-in-range
+  "how many numbers n exist, such that a <= n*k <= b, if a <= b, or vice versa"
+  [a b k]
+  ;; ceil(a/k) <= n <= floor(b/k)
+  (range-len (ceil-div (min a b) k)
+             (floor-div (max a b) k)))
+
 (defn step [n cmd k]
-  (let [m (if (= "R" cmd) k (- k))]
-    (mod (+ n m) 100)))
+  (let [m (if (= "R" cmd) k (- k))
+        d 100
+        next (+ n m)]
+    [(mod next d) ;; next position
+     ;; how many multiplies of 100 where met, excluding n
+     (- (multiples-in-range n next d)
+        (if (zero? (mod n d)) 1 0))
+     ]))
 
 (defn exec [init cmds]
-  (loop [n init, cmds cmds, res [init]]
+  (loop [n init, cmds cmds, res [[init 0]]]
     (if (empty? cmds) res
         (let [[[cmd k] & rest] cmds
-              next (step n cmd k)]
-          (recur next rest (conj res next))))))
+              [next m :as r] (step n cmd k)]
+          (recur next rest (conj res r))))))
 
 (defn part1 [cmds]
   (->> (exec 50 cmds)
-       (filter #(= 0 %)) count))
+       (filter #(= 0 (first %))) count))
+
+(defn part2 [cmds]
+  (->> (exec 50 cmds)
+       (map second)
+       (reduce + 0)))
 
 (comment
-(def ex (slurp (io/resource "day01/example.txt")))
-(->> (parse ex) (exec 50))
-;; => [50 82 52 0 95 55 0 99 0 14 32]
-(->> (parse ex) (exec 50)
-     (filter #(= 0 %)) count)
+  (def ex (slurp (io/resource "day01/example.txt")))
+  (->> (parse ex) (exec 50))
+  ;; => [50 82 52 0 95 55 0 99 0 14 32]
+  (->> (parse ex) (exec 50)
+       (filter #(= 0 %)) count)
 
-(->> (io/resource "day01/input.txt")
-     slurp parse (exec 50)
-     (filter #(= 0 %)) count)
-)
+  (->> (io/resource "day01/input.txt")
+       slurp parse (exec 50)
+       (filter #(= 0 %)) count)
+  )
 
 (defn run [path]
-(let [cmds (parse (slurp (io/resource path)))]
-  (println "part1:" (part1 cmds))
-  true))
+  (let [cmds (parse (slurp (io/resource path)))]
+    (println "part1:" (part1 cmds))
+    (println "part2:" (part2 cmds))
+    true))
--- advent-of-code/aoc2025/clj/test/aoc/day01_test.clj
+++ advent-of-code/aoc2025/clj/test/aoc/day01_test.clj
@@ -3,12 +3,37 @@
             [clojure.test :refer :all]
             [clojure.java.io :as io]))
 
+(defn multiples-in-range-ref
+  "how many numbers n exist, such that a <= n*k <= b"
+  [a b k]
+  (if (< b a) (recur b a k)
+      (->> (range a (inc b))
+           (filter #(zero? (rem % k)))
+           count)))
+
+(deftest test-multiples-in-range
+  (testing "test ref"
+    (is (= 5 (multiples-in-range-ref -1 3 1)))
+    (is (= 3 (multiples-in-range-ref 1 3 1)))
+    (is (= 4 (multiples-in-range-ref -3 5 2)))
+    (is (= 5 (multiples-in-range-ref -4 5 2)))
+    (is (= 5 (multiples-in-range-ref -3 6 2)))
+    (is (= 0 (multiples-in-range-ref 1 4 5))))
+  (testing "test actual impl"
+    (for [k (range 1 8)
+          a (range -10 11)
+          b (range -10 11)]
+      (do (println "[k a b] =" [k a b])
+          (is (= (multiples-in-range-ref a b k) (multiples-in-range a b k)))))))
+
 (deftest test-exec
   (testing "Should work on example"
     (let [s (slurp (io/resource "day01/example.txt"))
           cmds (parse s)]
       (is (= [50 82 52 0 95 55 0 99 0 14 32]
-             (exec 50 cmds))))))
+             (map first (exec 50 cmds))))
+      (is (= [0 1 0 1 0 1 1 0 1 0 1]
+             (map second (exec 50 cmds)))))))
 
 
 
