72f99c3e8154ba749b5cdea0f96e41f4d2cbe1ce
anton0xf <anton0xf@gmail.com>
2025-12-11 10:12:51 +0300

    scala/stepik/basics3: 3.11.4. comparing tuple (or not)

---
+++ scala/stepik/basics3/src/u3/l11/S04CmpTup.scala
@@ -0,0 +1,15 @@
+package u3.l11
+
+object S04CmpTup {
+
+  private def f(arg1: Char, arg2: Char): Unit = {
+    val tuple = List[(Char, Char) => Boolean]((_ == _), (_ != _), (_ > _), (_ >= _), (_ < _), (_ <= _))
+    val res = tuple.map(f => f(arg1, arg2))
+    println(res.mkString("(", ",", ")"))
+  }
+
+  @main
+  def main(): Unit = {
+    f('a', 'b')
+  }
+}
